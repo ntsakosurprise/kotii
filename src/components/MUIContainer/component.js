d712357adaf0045e84ebc6e668214ad1da0129f4
ntsakosurprise <mashelesepru@gmail.com>
2022-01-19 15:08:41 +0200

    chore(docs): updates MUIContainer

--- src/components/MUIContainer/component.js
+++ src/components/MUIContainer/component.js
@@ -1,8 +1,13 @@
 import * as React from "react";
 import Container from "@mui/material";
+import Grid from "@mui/material";
 
 const MUIContainer = () => {
-  return <Container>{children}</Container>;
+  return (
+    <Container container spacing={2}>
+      <Grid>{children}</Grid>
+    </Container>
+  );
 };
 
 export default MUIContainer;
