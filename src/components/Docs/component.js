ca83efcbd5dbf82cc424627ef8f76b3a995862d9
ntsakosurprise <mashelesepru@gmail.com>
2022-01-23 07:31:08 +0200

    chore(docs): adds markdown css

--- src/components/Docs/component.js
+++ src/components/Docs/component.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../Sidebar/component";
 import DocsMain from "../DocsMain/component";
-import Markdown from "markdown-to-jsx";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const Docs = ({ children, mdContent = {} }) => {
   console.log("docs.md;;", mdContent);
@@ -9,7 +10,11 @@ const Docs = ({ children, mdContent = {} }) => {
     <>
       <Sidebar />
       <DocsMain>
-        <Markdown options={{ forceBlock: true }} children={mdContent.md} />
+        <ReactMarkdown
+          remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+          children={mdContent.md}
+          className="markdown"
+        />
         {/* {children} */}
       </DocsMain>
     </>
