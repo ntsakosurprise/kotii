1bcf1148f0c4688347ce9cc4eba24322f6b41bcd
ntsakosurprise <mashelesepru@gmail.com>
2022-07-13 15:28:21 +0200

    refactor: creates engine-locking files to enforce consistent tooling

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
   parserPreset: "conventional-changelog-conventionalcommits",
   rules: {
     "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 300],
     "footer-leading-blank": [1, "always"],
     "footer-max-line-length": [2, "always", 100],
     "header-max-length": [2, "always", 100],
