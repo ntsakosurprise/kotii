eb600744f6ac7f066795f6c8f70c083e331355a0
ntsakosurprise <mashelesepru@gmail.com>
2022-07-18 23:39:29 +0200

    refactor(nextgen-docs-languages): exports language resources

---
+++ packages/nextgen-docs-languages/src/index.js
@@ -0,0 +1,4 @@
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import { LanguageProvider, useLanguage } from "./context/language-provider";
+
+export { LanguageProvider, useLanguage, LanguageSwitcher };
