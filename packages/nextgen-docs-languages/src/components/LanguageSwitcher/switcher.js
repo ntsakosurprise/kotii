960cf810e97de3da8d12c6f65c9d4201af30b7a8
ntsakosurprise <mashelesepru@gmail.com>
2022-07-18 23:40:29 +0200

    refactor(nextgen-docs-languages): adds react-select to the LanguageSwitcher component

--- packages/nextgen-docs-languages/src/components/LanguageSwitcher/switcher.js
+++ packages/nextgen-docs-languages/src/components/LanguageSwitcher/switcher.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
+import { useLanguage } from "../../context/language-provider";
+
+const options = [
+  { value: "en", label: "English" },
+  { value: "ts", label: "Tsonga" },
+];
 
 const LanguageSwitcher = () => {
+  const { changeCurrentLanguage, language } = useLanguage;
+  const [selectedOption, setSelectedOption] = useState(language);
+
+  const switchLanguage = (value) => {
+    console.log("the props;;", value);
+    changeCurrentLanguage();
+    setSelectedOption();
+  };
+
   return (
     <div>
-      <select></select>
+      <Select
+        defaultValue={selectedOption}
+        onChange={(e) => {
+          switchLanguage(e.target.value);
+        }}
+        options={options}
+      />
     </div>
   );
 };
