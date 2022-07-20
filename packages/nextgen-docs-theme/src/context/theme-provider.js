e3bcb06db0d58687555a322e80323c21b7eabb2f
ntsakosurprise <mashelesepru@gmail.com>
2022-07-20 17:03:50 +0200

    refactor(nextgen-docs-theme): partially implments theme provider

---
+++ packages/nextgen-docs-theme/src/context/theme-provider.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+const ThemeContext = React.createContext(null);
+
+export const LanguageProvider = (props) => {
+  return <ThemeContext.Provider>{props.children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => React.useContext(ThemeContext);
