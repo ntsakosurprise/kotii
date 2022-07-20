fac4b6f5459a8c2f9ad342bc2ea3d8013cbf8e29
ntsakosurprise <mashelesepru@gmail.com>
2022-07-20 06:20:06 +0200

    refactor(nextgen-docs-languages): removes TestComponent

--- packages/nextgen-docs-languages/src/index.js
+++ packages/nextgen-docs-languages/src/index.js
@@ -1,20 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState } from "react";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import { LanguageProvider, useLanguage } from "./context/language-provider";
-console.log("LANGUAGE pROVIDER", LanguageProvider);
 
-const TestComponent = () => {
-  const { test, setTest } = useState("MY TEST");
-  return <p>LOOK AT ME NOW, I'M A TESTER{test}</p>;
-};
-
-export { LanguageProvider, useLanguage, LanguageSwitcher, TestComponent };
-
-// console.log("LANGUAGE pROVIDER", LanguageProvider);
-// export const test = () => {
-//   console.log("i'm the test function");
-// };
-// export const test2 = () => {
-//   console.log("number2");
-// };
+export { LanguageProvider, useLanguage, LanguageSwitcher };
