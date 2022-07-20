266c4107760a2ac97365c731be7cd2cd71a968c7
ntsakosurprise <mashelesepru@gmail.com>
2022-07-20 05:56:13 +0200

    refactor(nextgen-docs-languages): references config file by index file

--- packages/nextgen-docs-languages/src/context/language-provider.js
+++ packages/nextgen-docs-languages/src/context/language-provider.js
@@ -2,12 +2,13 @@
 // import i18next from "i18next";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { initialize } from "../config";
+import { initialize } from "../config/index";
 const LanguageContext = React.createContext(null);
 
 export const LanguageProvider = (props) => {
   const { language, changeLanguage } = useState("en");
   const { t, i18n } = useTranslation();
+  console.log("the children;;", props);
 
   useEffect(() => {
     initialize();
