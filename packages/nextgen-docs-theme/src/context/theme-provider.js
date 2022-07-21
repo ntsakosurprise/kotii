746fcd8b3039475056b472603326aca9dad8ae91
ntsakosurprise <mashelesepru@gmail.com>
2022-07-22 01:15:47 +0200

    refactor(nextgen-docs-theme): update provider name and implements useTheme hook

--- packages/nextgen-docs-theme/src/context/theme-provider.js
+++ packages/nextgen-docs-theme/src/context/theme-provider.js
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import { logStoredThemesStatus } from "Config";
+import { useTheme } from "Hooks";
+import React, { useEffect } from "react";
 const ThemeContext = React.createContext(null);
 
-export const LanguageProvider = (props) => {
-  return <ThemeContext.Provider>{props.children}</ThemeContext.Provider>;
+export const ThemeProvider = (props) => {
+  const [theme, isThemeLoaded, changeTheme] = useTheme();
+  useEffect(() => {
+    logStoredThemesStatus();
+  });
+  return (
+    <ThemeContext.Provider value={(theme, isThemeLoaded, changeTheme)}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useThemeContext = () => React.useContext(ThemeContext);
