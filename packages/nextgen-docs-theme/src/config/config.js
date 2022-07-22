099f279539887922b1361618c320cf1d3a75e6c1
ntsakosurprise <mashelesepru@gmail.com>
2022-07-22 20:00:08 +0200

    refactor(nextgen-docs-theme): implements default theme setting mechanism

--- packages/nextgen-docs-theme/src/config/config.js
+++ packages/nextgen-docs-theme/src/config/config.js
@@ -1,7 +1,11 @@
 import { getFromStorage, setInStorage } from "Utilities";
 import { themes } from "./themes";
 
+const processDefaultTheme = () => {
+  if (!getFromStorage("theme")) setInStorage("theme", themes.calm);
+};
 setInStorage("themes", themes);
+processDefaultTheme();
 
 export const logStoredThemesStatus = () => {
   if (getFromStorage("themes")) {
