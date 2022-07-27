5072438fb1c3ae9f3308582de53ec4b6cf3ee5ab
ntsakosurprise <mashelesepru@gmail.com>
2022-07-27 06:21:06 +0200

    feat(mymtn-nextgen-docs-ui): creates a markdown component

---
+++ packages/mymtn-nextgen-docs-ui/src/components/docs/Markdown/component.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export const Markdown = () => {
+  return (
+    <div></div>
+    // <Carousel />
+    //   <CarouselSlick
+    //     settings={settings}
+    //     items={[...items]}
+    //     shouldShowHead={true}
+    //   />
+  );
+};
