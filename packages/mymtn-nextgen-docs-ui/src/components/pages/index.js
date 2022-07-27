a5c7e34a477a42af01b1e828fb602619a5e484c5
ntsakosurprise <mashelesepru@gmail.com>
2022-07-27 09:55:24 +0200

    feat(mymtn-nextgen-docs-ui): updates GetStarted component and its related resources

--- packages/mymtn-nextgen-docs-ui/src/components/pages/index.js
+++ packages/mymtn-nextgen-docs-ui/src/components/pages/index.js
@@ -1,8 +1,7 @@
 import About from "./About/component";
-import { GetStarted } from "./GetStarted/component";
 import HelpSupport from "./HelpSupport/component";
 import { Home } from "./Home/component";
 import Personal from "./Personal/component";
 import ProductServices from "./ProductServices/component";
 
-export { About, Home, Personal, HelpSupport, ProductServices, GetStarted };
+export { About, Home, Personal, HelpSupport, ProductServices };
