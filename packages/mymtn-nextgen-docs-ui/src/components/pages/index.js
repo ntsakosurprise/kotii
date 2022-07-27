a1af87f48900fffe3206608b085a981ad370452c
ntsakosurprise <mashelesepru@gmail.com>
2022-07-27 06:27:30 +0200

    feat(mymtn-nextgen-docs-ui): imports and exports GetStarted component

--- packages/mymtn-nextgen-docs-ui/src/components/pages/index.js
+++ packages/mymtn-nextgen-docs-ui/src/components/pages/index.js
@@ -1,7 +1,8 @@
 import About from "./About/component";
+import { GetStarted } from "./GetStarted/component";
+import HelpSupport from "./HelpSupport/component";
 import { Home } from "./Home/component";
 import Personal from "./Personal/component";
-import HelpSupport from "./HelpSupport/component";
 import ProductServices from "./ProductServices/component";
 
-export { About, Home, Personal, HelpSupport, ProductServices };
+export { About, Home, Personal, HelpSupport, ProductServices, GetStarted };
