4819c723118eaea41e917fbb0ee2c10e0d0b4e9b
ntsakosurprise <mashelesepru@gmail.com>
2022-07-27 14:36:18 +0200

    refactor(nextgen-docs-theme): updates webpack to reflect proper module name

--- packages/nextgen-docs-theme/webpack.config.js
+++ packages/nextgen-docs-theme/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: "index.js",
     chunkFilename: "[id].js",
     publicPath: "",
-    library: "nextgen-docs-languages",
+    library: "nextgen-docs-theme",
     libraryTarget: "umd",
   },
   externals: {
