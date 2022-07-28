c02c2d001be5fe455c8114336939bbbb665c2d64
ntsakosurprise <mashelesepru@gmail.com>
2022-07-28 15:36:56 +0200

    feat(nextgen-docs-markdown): changes function from arrow to old syntax

--- packages/nextgen-docs-markdown/lib/loader.js
+++ packages/nextgen-docs-markdown/lib/loader.js
@@ -2,8 +2,23 @@ const { parseMarkdown } = require("./markdownParser");
 
 console.log(parseMarkdown);
 
-module.exports = async () => {
+module.exports = function (source) {
+  console.log("MarkDownFolder", rePath);
+  console.log("the source;;", source);
   this.getLogger();
   const rePath = this.resourcePath;
   console.log("MarkDownFolder", rePath);
+  console.log("the source;;", source);
+  const docs = {
+    doc1: "I'm doc 1",
+    doc2: "I'm doc 2",
+  };
+
+  const loaded = `
+
+   export const docs = ${JSON.stringify(docs, null, 2)}
+  
+  `;
+
+  return loaded;
 };
