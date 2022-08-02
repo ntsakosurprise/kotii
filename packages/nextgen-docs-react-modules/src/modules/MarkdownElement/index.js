5eb292e4f26e5e2e7c6d21511b5a6f810c32dce7
ntsakosurprise <mashelesepru@gmail.com>
2022-08-02 21:16:40 +0200

    feat(nextgen-docs-react-modules): updates components styles

--- packages/nextgen-docs-react-modules/src/modules/MarkdownElement/index.js
+++ packages/nextgen-docs-react-modules/src/modules/MarkdownElement/index.js
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 const StyledMarkdown = styled("div")(() => ({
-  "& h1": {},
+  "& h1": { color: "red", margin: "10px 0", fontWeight: 800 },
+  "& table": { minWidth: "600px", border: "2px solid #2a2f36" },
+  "& table thead": {
+    background: "#2a2f36",
+    color: "#fff",
+    textAlign: "left",
+    fontWeight: 600,
+  },
+  "& table thead th": { fontWeight: 600 },
+  "& table td,\ntable th": {
+    padding: "10px",
+    fontSize: "16px",
+    fontWeight: 400,
+  },
 }));
 
 const MarkdownElement = ({ children }) => {
