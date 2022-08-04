9f7ae0903f2c764c39150c444df594f52a8d6b33
ntsakosurprise <mashelesepru@gmail.com>
2022-08-04 23:12:17 +0200

    feat(nextgen-docs-react-modules): creates DemoSource content

--- packages/nextgen-docs-react-modules/src/modules/Demo/index.js
+++ packages/nextgen-docs-react-modules/src/modules/Demo/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable react/display-name */
 import React from "react";
+import DemoSource from "./source";
 import DemoToolbar from "./toolbar";
 
 const Demo = () => {
-  return <DemoToolbar />;
+  return (
+    <>
+      <DemoToolbar />
+      <DemoSource />
+    </>
+  );
 };
 
 export default Demo;
