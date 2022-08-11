291c538046209803beff13c748ea269c4b6f3232
ntsakosurprise <mashelesepru@gmail.com>
2022-08-11 14:08:44 +0200

    feat(nextgen-docs-react-modules): updates showcase component for demos

--- packages/nextgen-docs-react-modules/src/modules/FlexSandbox/showcase.js
+++ packages/nextgen-docs-react-modules/src/modules/FlexSandbox/showcase.js
@@ -1,21 +1,14 @@
 import styled from "styled-components";
 
 const ShowCase = styled("div")(() => ({
-  // background: "#eee",
-  background: "#001E3C;",
+  background: "#eee",
   border: "1px solid #9684A3",
   borderRadius: "10px",
-  // padding: "10px",
+  padding: "10px",
   letterSpacing: ".5px",
   fontSize: "10pt",
   color: "#96b38a",
   fontWeight: "bold",
-  padding: "5px",
-  paddingTop: "50px",
-  paddingBottom: "50px",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
 }));
 
 export { ShowCase };
