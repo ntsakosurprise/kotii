9ac26c548222acf71ae53c5a0e4abfd5e2d69f05
ntsakosurprise <mashelesepru@gmail.com>
2022-08-14 21:33:41 +0200

    feat(nextgen-docs-react-modules): updates source text and props

--- packages/nextgen-docs-react-modules/src/modules/Demo/source.js
+++ packages/nextgen-docs-react-modules/src/modules/Demo/source.js
@@ -1,27 +1,31 @@
 /* eslint-disable react/prop-types */
 
 import React from "react";
-import styled from "styled-components";
+//import styled from "styled-components";
 // import DocsComponent from "../DocsComponent/component";
 import { StyledMarkdown } from "../StyledMarkdown";
+import LiveEditor from "./liveeditor";
 const prism = require("prismjs");
 require("prismjs/components/prism-jsx");
 
-const LiveEditor = styled("div")(() => ({
-  background: "#20354A",
-  width: "100%",
-  color: "green",
-}));
+// const LiveEditor = styled("div")(() => ({
+//   background: "#20354A",
+//   width: "100%",
+//   color: "green",
+// }));
 
-const DemoSource = ({ showSource, docs, liveEdit }) => {
+const DemoSource = ({ showSource, docs, liveEdit, runCode, textEditor }) => {
   console.log("docs;;;", docs);
   console.log("liveedit;;;", liveEdit);
+  console.log("tHE TEXT EDITOR;;;", textEditor);
   if (!showSource) return null;
   if (liveEdit)
     return (
-      <LiveEditor>
-        <p>I am the LiveEditor</p>
-      </LiveEditor>
+      <LiveEditor
+        content={docs.content}
+        textEditor={textEditor}
+        run={runCode}
+      />
     );
   return (
     <StyledMarkdown>
