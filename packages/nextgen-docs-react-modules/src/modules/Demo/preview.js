3348dd4d3ca2d9cbb3d21b6a96e5459a72881e9e
ntsakosurprise <mashelesepru@gmail.com>
2022-08-14 21:34:14 +0200

    feat(nextgen-docs-react-modules): forwards ref to Preview

---
+++ packages/nextgen-docs-react-modules/src/modules/Demo/preview.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const Preview = (props, ref) => {
+  return (
+    <div ref={(previewNode) => (previewNode = ref)}>I am the PreviewRef</div>
+  );
+};
+
+export default React.forwardRef(Preview);
