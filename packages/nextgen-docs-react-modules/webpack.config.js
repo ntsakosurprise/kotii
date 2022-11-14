54ad87de1802ef23909fd072290b8e2cac0c6a13
ntsakosurprise <mashelesepru@gmail.com>
2022-11-14 19:44:10 +0200

    feat(nextgen-docs-react-modules): set source-map for developmen

--- packages/nextgen-docs-react-modules/webpack.config.js
+++ packages/nextgen-docs-react-modules/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     // library: "nextgen-docs-react-modules",
     // libraryTarget: "umd",
   },
+  devtool: "source-map",
   // externals: {
   //   react: {
   //     root: "React",
