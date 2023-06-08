e3a922342d98f73f1af9156c46dc9e3cfddbb565
ntsakosurprise <mashelesepru@gmail.com>
2023-06-08 05:16:17 +0200

    chore(kotii-markdown): calls parseMarkdwon to begin the parsing process

--- packages/kotii-markdown/lib/loader.js
+++ packages/kotii-markdown/lib/loader.js
@@ -3,14 +3,15 @@ const { parseMarkdown } = require("./markdownParser");
 console.log(parseMarkdown);
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function (source) {
+module.exports = function (markdown) {
   this.getLogger();
-  // const reContext = this.context;
+  const reContext = this.context;
   // const rePath = this.resourcePath;
   // console.log("MarkDownFolder", rePath);
-  //   console.log("the source;;", reContext);
+  console.log("the source;;", reContext);
+  parseMarkdown(markdown);
   const docs = {
-    content: source,
+    content: markdown,
   };
 
   const loaded = `
