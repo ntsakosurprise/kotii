8ffd1a7ba32d8296bc7899459d9e4de5405cfe77
ntsakosurprise <mashelesepru@gmail.com>
2023-06-10 00:49:11 +0200

    chore(kotii-markdown): adds one more test for special content extraction

--- packages/kotii-markdown/lib/markdonwParser.test.js
+++ packages/kotii-markdown/lib/markdonwParser.test.js
@@ -1,5 +1,10 @@
 import { keyValueString, markdown } from "./constants";
-import { extractMetaData, extractMetaKeyPairs } from "./markdownParser";
+import {
+  extractDescription,
+  extractMetaData,
+  extractMetaKeyPairs,
+  extractSpecialContent,
+} from "./markdownParser";
 
 describe("Markdown Parser Extracters", () => {
   test("Should return a null given unmatched string", () => {
@@ -10,5 +15,13 @@ describe("Markdown Parser Extracters", () => {
   });
   test("Should return true given a string", () => {
     expect(extractMetaKeyPairs(keyValueString)).toBeTruthy();
+  });
+
+  test("Should return true given a string", () => {
+    expect(extractDescription(markdown)).toBeTruthy();
+  });
+
+  it("Should return matched special content given markdown string", () => {
+    expect(extractSpecialContent(markdown)).toBeTruthy();
   });
 });
