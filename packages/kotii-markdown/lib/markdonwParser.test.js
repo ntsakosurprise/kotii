5fb46cf529d142775c9ea0227cb72161150b8d39
ntsakosurprise <mashelesepru@gmail.com>
2023-06-09 19:39:56 +0200

    chore(kotii-markdown): implements function to extract key-value pairs for meta data

--- packages/kotii-markdown/lib/markdonwParser.test.js
+++ packages/kotii-markdown/lib/markdonwParser.test.js
@@ -1,5 +1,14 @@
-import { extractMetaData } from "./markdownParser";
+import { keyValueString, markdown } from "./constants";
+import { extractMetaData, extractMetaKeyPairs } from "./markdownParser";
 
-test("Should return a null given unmatched string", () => {
-  expect(extractMetaData("I'm the test string")).toBe(null);
+describe("Markdown Parser Extracters", () => {
+  test("Should return a null given unmatched string", () => {
+    expect(extractMetaData("I am the test string")).toBe(null);
+  });
+  test("Should return an array given matched string", () => {
+    expect(extractMetaData(markdown)).toBeTruthy();
+  });
+  test("Should return true given a string", () => {
+    expect(extractMetaKeyPairs(keyValueString)).toBeTruthy();
+  });
 });
