933ee599c681203737430267aae354c42adc9450
ntsakosurprise <mashelesepru@gmail.com>
2023-06-18 02:11:26 +0200

    chore(kotii-markdown): changes target to node in webpack

--- packages/kotii-markdown/webpack.config.js
+++ packages/kotii-markdown/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./lib/loader.js",
-  target: "web",
+  target: "node",
   output: {
     path: path.join(__dirname),
     filename: "index.js",
