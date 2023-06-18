5d7ee2b4f285b73ddabbdb8bef27856867c84b04
ntsakosurprise <mashelesepru@gmail.com>
2023-06-18 02:57:48 +0200

    chore(kotii-markdown): retrieve file information from the specified file path

--- packages/kotii-markdown/lib/loader.js
+++ packages/kotii-markdown/lib/loader.js
@@ -1,3 +1,6 @@
+const path = require("path");
+// import path from "path";
+//const fs = require("fs");
 const { parseMarkdown } = require("./markdownParser");
 
 console.log(parseMarkdown);
@@ -5,11 +8,23 @@ console.log(parseMarkdown);
 // eslint-disable-next-line no-unused-vars
 module.exports = function (markdown) {
   this.getLogger();
-  const reContext = this.context;
+  const resourceRootFolder = process.cwd(); // Get all resources root folder
+  const filePath = this.resourcePath; // Webpack, get filepath
+  const fileFolder = path.dirname(filePath); // Use file path to get file folder
+  const fileName = path.basename(filePath); // Get filename(including extension)
+  const fileExtension = path.extname(filePath); // Get file extension
+  const fileNamePlain = path.basename(filePath, fileExtension); // Get filename without extension
   // const rePath = this.resourcePath;
   // console.log("MarkDownFolder", rePath);
-  console.log("the source;;", reContext);
-  parseMarkdown(markdown);
+  console.log("resource root", resourceRootFolder);
+  console.log("file path;", filePath);
+  console.log("fileFolder", fileFolder);
+  console.log("fileName", fileName);
+  console.log("FileExtension", fileExtension);
+  console.log("FileNamePlain", fileNamePlain);
+  // console.log("The source BASE PATH", path.dirname(reContext));
+
+  //parseMarkdown(markdown);
   const docs = {
     content: markdown,
   };
