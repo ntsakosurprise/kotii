46eb22bd45db9a4d1c6518c37ac705a940dbf0db
ntsakosurprise <mashelesepru@gmail.com>
2023-07-01 01:35:07 +0200

    chore(kotii-react-modules):adds styling to standard component

--- packages/kotii-react-modules/src/modules/StandardComponent/index.js
+++ packages/kotii-react-modules/src/modules/StandardComponent/index.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
 import PropTtypes from "prop-types";
 import React from "react";
+import styled from "styled-components";
+
+const StyledStandardComponent = styled("div")(() => ({
+  width: "100%",
+}));
 
 const StandardComponent = ({ children }) => {
-  return <>{children}</>;
+  return <StyledStandardComponent>{children}</StyledStandardComponent>;
 };
 
 StandardComponent.propTypes = {
