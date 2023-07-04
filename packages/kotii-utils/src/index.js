e8093f15737c0d40fe7fecac4e8134ef0343cef3
ntsakosurprise <mashelesepru@gmail.com>
2023-07-05 00:05:44 +0200

    chore(kotii-utils): updates utils and updates package.json

---
+++ packages/kotii-utils/src/index.js
@@ -0,0 +1,12 @@
+import { getFromStorage, setInStorage } from "./localStorage";
+import {
+  capitalizeFirstLetter,
+  capitalizeLastLetter,
+} from "./textManipulations/index";
+
+export {
+  capitalizeFirstLetter,
+  capitalizeLastLetter,
+  getFromStorage,
+  setInStorage,
+};
