3d41a604889c98af308f3399da376b9282d135c2
ntsakosurprise <mashelesepru@gmail.com>
2023-07-27 04:55:50 +0200

    chore(kotii-ui): Creates test case for Page component

--- packages/kotii-ui/src/components/layout/Page/Page.test.tsx
+++ packages/kotii-ui/src/components/layout/Page/Page.test.tsx
@@ -2,14 +2,17 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import KPage from "./Page";
+import { KotiiThemeProvider } from "../../../context";
+import Page from "./Page";
 
 describe("Running Test for Page component", () => {
   test("Check if page component renders", () => {
     render(
-      <KPage kind="wide">
-        <p>My name</p>
-      </KPage>
+      <KotiiThemeProvider>
+        <Page kind="wide">
+          <p>My name</p>
+        </Page>
+      </KotiiThemeProvider>
     );
     // expect(
     //   screen.getByRole("button", { name: "Button marbella" })
--- packages/kotii-ui/src/components/layout/Page/Page.tsx
+++ packages/kotii-ui/src/components/layout/Page/Page.tsx
@@ -3,20 +3,17 @@ import React from "react";
 import styled from "styled-components";
 // import { BoxProps } from "./types";
 //import { BaseProps } from "../../../types";
-import { KotiiThemeProvider } from "../../../context";
 import { PageProps } from "./types";
 
 const WrappedPage = styled.div<PageProps>``;
 
 const Page: React.FC<PageProps> = ({ children, ...props }) => {
   return (
-    <KotiiThemeProvider>
-      <WrappedPage>
-        <Gpage {...props}>
-          <PageContent background="red">{children}</PageContent>
-        </Gpage>
-      </WrappedPage>
-    </KotiiThemeProvider>
+    <WrappedPage>
+      <Gpage {...props}>
+        <PageContent background="red">{children}</PageContent>
+      </Gpage>
+    </WrappedPage>
   );
 };
 
