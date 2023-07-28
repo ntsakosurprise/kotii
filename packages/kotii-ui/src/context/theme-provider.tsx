2187c59c2ea19b8bb8cca351660c61d5464e4b1c
ntsakosurprise <mashelesepru@gmail.com>
2023-07-28 04:48:46 +0200

    chore(kotii-ui): refactors theme provider

--- packages/kotii-ui/src/context/theme-provider.tsx
+++ packages/kotii-ui/src/context/theme-provider.tsx
@@ -1,23 +1,23 @@
 /* eslint-disable react/prop-types */
-// import { logStoredThemesStatus } from "Config";
-// import { deepMerge } from "grommet/utils";
-// import { useTheme } from "Hooks";
-import React from "react";
+import { deepMerge } from "grommet/utils";
+import React, { useEffect } from "react";
+import { logStoredThemesStatus } from "../config";
+import { useTheme } from "../hooks";
 // import { DemoSection } from "../components/ThemeSwitcher/DemoSection";
 // import { ThemeProvider } from "styled-components";
 
-import { Grommet } from "grommet";
+import { defaultProps as grommetTheme, Grommet, grommet } from "grommet";
 
 type ThemeProps = {
-  theme: { name: string };
-};
-
-const theme = {
-  theme: { name: "Surprise" },
+  theme: Object;
+  themes: [];
+  isThemeLoaded: boolean;
+  changeTheme: (currentTheme: any) => void;
+  grommetTheme: Object;
 };
 
 // Create ThemeContent
-const ThemeContext = React.createContext<ThemeProps>(theme);
+const ThemeContext = React.createContext<ThemeProps>({} as ThemeProps);
 
 // const myGrommetTheme = {
 //   global: {
@@ -28,25 +28,27 @@ const ThemeContext = React.createContext<ThemeProps>(theme);
 // };
 
 export const CustomThemeProvider = (props) => {
-  // const { theme, themes, isThemeLoaded, changeTheme } = useTheme();
-  // const [darkMode, setDarkMode] = React.useState(true);
+  const { theme, themes, isThemeLoaded, changeTheme } = useTheme();
+  const [darkMode, setDarkMode] = React.useState(true);
 
-  // console.log("currentTheme;;;", theme);
-  // console.log("CurrentThemes", themes);
-  // console.log(isThemeLoaded);
-  // console.log(props);
-  // useEffect(() => {
-  //   logStoredThemesStatus();
-  // }, []);
-  // useEffect(() => {
-  //   console.log("The updates theme;;;", theme);
-  //   console.log("Grommet", grommet);
-  // }, [theme]);
+  console.log("currentTheme;;;", theme);
+  console.log("CurrentThemes", themes);
+  console.log(isThemeLoaded);
+  console.log(props);
+  useEffect(() => {
+    logStoredThemesStatus();
+  }, []);
+  useEffect(() => {
+    console.log("The updates theme;;;", theme);
+    //console.log("Grommet", grommet);
+  }, [theme]);
   return (
-    <ThemeContext.Provider value={theme}>
+    <ThemeContext.Provider
+      value={{ theme, themes, isThemeLoaded, changeTheme, grommetTheme }}
+    >
       <Grommet
-      // theme={deepMerge(grommet, theme)}
-      // themeMode={darkMode ? "dark" : "light"}
+        theme={deepMerge(grommet, theme)}
+        themeMode={darkMode ? "dark" : "light"}
       >
         {props.children}
       </Grommet>
