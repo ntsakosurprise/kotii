dbabd4fddaf4f2fb795b4d5f13dd990262b3e4ca
ntsakosurprise <mashelesepru@gmail.com>
2023-07-29 06:11:50 +0200

    chore(kotii-ui): creates Nav component

---
+++ packages/kotii-ui/src/components/controls/Nav/Nav.tsx
@@ -0,0 +1,19 @@
+import { Nav as Gnav } from "grommet";
+import React from "react";
+import styled from "styled-components";
+
+// import { BoxProps } from "./types";
+//import { BaseProps } from "../../../types";
+import { PageHeaderProps } from "./types";
+
+const WrappedNav = styled.div<PageHeaderProps>``;
+
+const Nav: React.FC<PageHeaderProps> = ({ children, ...props }) => {
+  return (
+    <WrappedNav>
+      <Gnav {...props}>{children}</Gnav>
+    </WrappedNav>
+  );
+};
+
+export default Nav;
