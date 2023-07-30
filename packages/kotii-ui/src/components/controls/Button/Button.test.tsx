1f50574cd27be35a910880a376bf1db4c39d72e0
ntsakosurprise <mashelesepru@gmail.com>
2023-07-30 13:12:31 +0200

    chore(kotii-ui): creates Button component and replaces the old

--- packages/kotii-ui/src/components/controls/Button/Button.test.tsx
+++ packages/kotii-ui/src/components/controls/Button/Button.test.tsx
@@ -2,13 +2,20 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+import { DOM_BY_TEXT } from "../../../constants";
+import { KotiiThemeProvider } from "../../../context";
 import Button from "./Button";
 
-describe("Running Test for Marbella Button", () => {
-  test("Check Button Disabled", () => {
-    render(<Button text="Button marbella" disabled />);
-    expect(
-      screen.getByRole("button", { name: "Button marbella" })
-    ).toBeDisabled();
+describe("Running Test for Grid component", () => {
+  test("Check if Grid component renders", () => {
+    render(
+      <KotiiThemeProvider>
+        <Button />
+      </KotiiThemeProvider>
+    );
+    // expect(
+    //   screen.getByRole("button", { name: "Button marbella" })
+    // ).toBeDisabled();
+    expect(screen.getByText(DOM_BY_TEXT)).toBeInTheDocument();
   });
 });
