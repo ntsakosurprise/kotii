1c25ea81be483cda6c58265b982cda3fd5fc70e7
ntsakosurprise <mashelesepru@gmail.com>
2023-07-30 13:13:51 +0200

    chore(kotii-ui): updates types

--- packages/kotii-ui/src/types/index.tsx
+++ packages/kotii-ui/src/types/index.tsx
@@ -1,11 +1,28 @@
 import { BaseProps } from "./generic/interface";
 import {
   Align,
+  Background,
   Basis,
+  ButtonType,
   Corners,
   FontSizes,
   RangeType,
   Size,
+  SizeWithNone,
+  Target,
   Weight,
 } from "./generic/types";
-export { BaseProps, Corners, Size, Align, Basis, Weight, RangeType, FontSizes };
+export {
+  BaseProps,
+  Corners,
+  Size,
+  Align,
+  Basis,
+  Weight,
+  RangeType,
+  FontSizes,
+  ButtonType,
+  Background,
+  Target,
+  SizeWithNone,
+};
