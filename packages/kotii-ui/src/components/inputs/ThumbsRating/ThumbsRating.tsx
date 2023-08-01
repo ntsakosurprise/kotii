16c0d27b3277b0ea8318c1a75e30b723ceecbda4
ntsakosurprise <mashelesepru@gmail.com>
2023-08-01 23:01:21 +0200

    chore(kotii-ui): Creates a ThumbsRating component

---
+++ packages/kotii-ui/src/components/inputs/ThumbsRating/ThumbsRating.tsx
@@ -0,0 +1,18 @@
+import { ThumbsRating as GthumbsRating } from "grommet";
+import React from "react";
+import styled from "styled-components";
+// import { BoxProps } from "./types";
+//import { BaseProps } from "../../../types";
+import { PageHeaderProps } from "./types";
+
+const WrappedThumbsRating = styled.div<PageHeaderProps>``;
+
+const ThumbsRating: React.FC<PageHeaderProps> = ({ name, ...props }) => {
+  return (
+    <WrappedThumbsRating name={name}>
+      <GthumbsRating {...props} name={name} />
+    </WrappedThumbsRating>
+  );
+};
+
+export default ThumbsRating;
