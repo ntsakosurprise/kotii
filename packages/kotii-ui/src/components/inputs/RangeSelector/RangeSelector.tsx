d1670851c55edfadc02c293d3bc1e90c65067254
ntsakosurprise <mashelesepru@gmail.com>
2023-08-01 22:58:14 +0200

    chore(kotii-ui): Creates a RangeSelector component

---
+++ packages/kotii-ui/src/components/inputs/RangeSelector/RangeSelector.tsx
@@ -0,0 +1,19 @@
+import { RangeSelector as GrangeSelector } from "grommet";
+import React from "react";
+import styled from "styled-components";
+
+// import { BoxProps } from "./types";
+//import { BaseProps } from "../../../types";
+import { PageHeaderProps } from "./types";
+
+const WrappedRangeSelector = styled.div<PageHeaderProps>``;
+
+const RangeSelector: React.FC<PageHeaderProps> = ({ ...props }) => {
+  return (
+    <WrappedRangeSelector>
+      <GrangeSelector {...props} />
+    </WrappedRangeSelector>
+  );
+};
+
+export default RangeSelector;
