667f9673d36ee41de4d8482a10cc7630a96a9b10
ntsakosurprise <mashelesepru@gmail.com>
2023-08-01 22:59:41 +0200

    chore(kotii-ui): Creates a StarRating component

---
+++ packages/kotii-ui/src/components/inputs/StarRating/StarRating.tsx
@@ -0,0 +1,18 @@
+import { StarRating as GstarRating } from "grommet";
+import React from "react";
+import styled from "styled-components";
+// import { BoxProps } from "./types";
+//import { BaseProps } from "../../../types";
+import { PageHeaderProps } from "./types";
+
+const WrappedStarRating = styled.div<PageHeaderProps>``;
+
+const StarRating: React.FC<PageHeaderProps> = ({ name, ...props }) => {
+  return (
+    <WrappedStarRating name={name}>
+      <GstarRating {...props} name={name} />
+    </WrappedStarRating>
+  );
+};
+
+export default StarRating;
