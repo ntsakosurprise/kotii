03a96c6c0af4f4cca55b9e3a1bc9be631a052aa1
ntsakosurprise <mashelesepru@gmail.com>
2023-08-05 06:27:45 +0200

    chore(kotii-ui): Creates DataTableGroupBy component

---
+++ packages/kotii-ui/src/components/data/DataTableColumns/types.ts
@@ -0,0 +1,7 @@
+import { BaseProps } from "../../../types";
+export interface PageHeaderProps extends BaseProps {
+  gridArea?: string;
+
+  drop: boolean;
+  options: (string | { property: string; label: string })[];
+}
