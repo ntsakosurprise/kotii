3721ff91d99059f04e8fcaf8b336420d46c09a28
ntsakosurprise <mashelesepru@gmail.com>
2023-08-14 22:42:33 +0200

    chore(kotii-ui): removes KotiiProvider on Header component

--- packages/kotii-ui/src/components/layout/Header/Header.tsx
+++ packages/kotii-ui/src/components/layout/Header/Header.tsx
@@ -2,7 +2,6 @@ import { Header as Gheader } from "grommet";
 import React from "react";
 import styled from "styled-components";
 // import { BoxProps } from "./types";
-import { KotiiThemeProvider } from "../../../context";
 import { BaseProps } from "../../../types";
 
 const WrappedHeader = styled.div<BaseProps>``;
@@ -13,11 +12,9 @@ const Header: React.FC<BaseProps> = ({
   ...props
 }) => {
   return (
-    <KotiiThemeProvider>
-      <WrappedHeader>
-        <Gheader {...props}>{children}</Gheader>
-      </WrappedHeader>
-    </KotiiThemeProvider>
+    <WrappedHeader>
+      <Gheader {...props}>{children}</Gheader>
+    </WrappedHeader>
   );
 };
 
