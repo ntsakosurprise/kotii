4c3ea83d997c14c9d6c9f7625358a6a721b46cd1
ntsakosurprise <mashelesepru@gmail.com>
2023-08-16 05:27:56 +0200

    chore(kotii-ui): updates shapes to include handler for circle shape

--- packages/kotii-ui/src/components/shapes/utils/shapes.ts
+++ packages/kotii-ui/src/components/shapes/utils/shapes.ts
@@ -1,5 +1,6 @@
 import { getDefaultValue } from "./getters";
 import { DoWidthHeightType } from "./types";
+const BORDER_RADIUS: string = "50%";
 export const squareWidthHeight = (width, height, shape): DoWidthHeightType => {
   if (!width && !height)
     return {
@@ -9,4 +10,17 @@ export const squareWidthHeight = (width, height, shape): DoWidthHeightType => {
   if (!width) return { width: height, height };
   if (!height) return { width, height: width };
   return { width, height };
+};
+
+export const circleWidthHeight = (width, height, shape): DoWidthHeightType => {
+  let borderRadius = BORDER_RADIUS;
+  if (!width && !height)
+    return {
+      width: getDefaultValue("width", true),
+      height: getDefaultValue("height", true),
+      borderRadius,
+    };
+  if (!width) return { width: height, height, borderRadius };
+  if (!height) return { width, height: width, borderRadius };
+  return { width, height, borderRadius };
 };
