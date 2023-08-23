aca8a3497c4dffd86b18b5ef581cead87362bb88
ntsakosurprise <mashelesepru@gmail.com>
2023-08-23 05:07:58 +0200

    chore(kotii-ui): adds rull to match graphic assets to webpack

--- packages/kotii-ui/webpack.config.js
+++ packages/kotii-ui/webpack.config.js
@@ -64,6 +64,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset/resource",
+      },
       // {
       //   test: /\.scss$/,
       //   use: ["style-loader", "css-loader", "sass-loader"],
