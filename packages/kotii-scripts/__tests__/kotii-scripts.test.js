226d0a4b5a6aaf4687733e8b198e3603e681298b
ntsakosurprise <mashelesepru@gmail.com>
2023-10-03 05:49:01 +0200

    chore(kotii-scripts): removes lerna generated files

--- packages/kotii-scripts/__tests__/kotii-scripts.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const kotiiScripts = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(kotiiScripts(), 'Hello from kotiiScripts');
-console.info("kotiiScripts tests passed");
--- packages/kotii-scripts/lib/kotii-scripts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = kotiiScripts;
-
-function kotiiScripts() {
-    return "Hello from kotiiScripts";
-}
