48e51f14af79ba834e80b911da2805534d020b3c
ntsakosurprise <mashelesepru@gmail.com>
2023-10-03 14:27:28 +0200

    chore(kotii-scripts): updates interpreter plugin

--- packages/kotii-scripts/app.js
+++ packages/kotii-scripts/app.js
@@ -1,6 +1,5 @@
-#!/usr/bin/env node 
-process.argv.push('cli') 
-
-const plugins = require('./plugins/index')
-require('anzii')(plugins)
+#!/usr/bin/env node
+process.argv.push("cli");
+const plugins = require("./plugins/index");
+require("anzii")(plugins);
 // console.log(anzii)
