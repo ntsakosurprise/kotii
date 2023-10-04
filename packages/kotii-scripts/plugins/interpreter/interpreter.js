a370c9fa990c63aedd4c4e8750b76aa5a9019b61
ntsakosurprise <mashelesepru@gmail.com>
2023-10-04 03:03:07 +0200

    chore(kotii-scripts): formats the interpreter

--- packages/kotii-scripts/plugins/interpreter/interpreter.js
+++ packages/kotii-scripts/plugins/interpreter/interpreter.js
@@ -1,49 +1,35 @@
-const methods = require("./methods")
+const methods = require("./methods");
 
-const arg = require('arg') 
-const chalk = require('chalk') 
-const figlet = require('figlet') 
-const cleya = require('clear') 
-const inquirer = require('inquirer')
+const arg = require("arg");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const cleya = require("clear");
+const inquirer = require("inquirer");
 
-
-class Interpreter{
-  
-  
-  constructor(pao){
-
-    this.pao = pao 
-    this.arg = arg
-    this.chalk = chalk 
-    this.figlet = figlet
-    this.clear = cleya
-    this.inquirer = inquirer
+class Interpreter {
+  constructor(pao) {
+    this.pao = pao;
+    this.arg = arg;
+    this.chalk = chalk;
+    this.figlet = figlet;
+    this.clear = cleya;
+    this.inquirer = inquirer;
     this.commands = {
-    	  
-    	  ["create-anzii-app"]: "create-anzii-app",
-    	 
-    }
-    
-   
-     this.init = methods.init
-     this.handleInterpreterCliInput = methods.handleInterpreterCliInput 
-     this.handlePromptUser = methods.handlePromptUser
-     this.handleCommands = methods.handleCommands 
-     this.getFeedback = methods.getFeedback 
-     this.prompt = methods.prompt 
-     this.outPut = methods.outPut 
-     this.showAvailableCommands = methods.showAvailableCommands 
-     this.createAnziiAppCommand = methods.createAnziiAppCommand 
-     this.helpCommand = methods.helpComand 
-     this.versionCommand = methods.versionCommand
+      ["create-anzii-app"]: "create-anzii-app",
+    };
 
-     
-
+    this.init = methods.init;
+    this.handleInterpreterCliInput = methods.handleInterpreterCliInput;
+    this.handlePromptUser = methods.handlePromptUser;
+    this.handleCommands = methods.handleCommands;
+    this.getFeedback = methods.getFeedback;
+    this.prompt = methods.prompt;
+    this.outPut = methods.outPut;
+    this.showAvailableCommands = methods.showAvailableCommands;
+    this.createAnziiAppCommand = methods.createAnziiAppCommand;
+    this.helpCommand = methods.helpComand;
+    this.versionCommand = methods.versionCommand;
   }
-
-
-  
-
 }
 
-module.exports = Interpreter
+module.exports = Interpreter;
