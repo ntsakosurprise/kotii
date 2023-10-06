22e13e80883ed3038179a01677122acc59506a73
ntsakosurprise <mashelesepru@gmail.com>
2023-10-06 11:33:36 +0200

    chore(kotii-scripts:wbpconfig):  sets methods in class

--- packages/kotii-scripts/plugins/wbpconfig/wbpconfig.js
+++ packages/kotii-scripts/plugins/wbpconfig/wbpconfig.js
@@ -3,15 +3,19 @@
  */
 const methods = require("./methods");
 const webPackConfig = require("./webpack.config");
+const webpack = require("webpack");
 
 class WebpackConfig {
   constructor(pao) {
     this.pao = pao;
     this.webPackConfig = webPackConfig;
+    this.webpack = webpack;
 
     this.init = methods.init;
     this.handleWebpackConfig = methods.handleWebpackConfig;
     this.configureWebPack = methods.configureWebPack;
+    this.giveWebpackFunction = methods.giveWebpackFunction;
+    this.setContextEnv = methods.setContextEnv;
   }
 }
 
