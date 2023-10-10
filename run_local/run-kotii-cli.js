eb9b23d38472ecedb23f21617d06709c9166a5ee
ntsakosurprise <mashelesepru@gmail.com>
2023-10-10 06:43:37 +0200

    chore(kotii-cli): rename kotii clie run function

--- run_local/run-kotii-cli.js
+++
@@ -1,2 +0,0 @@
-const createTarball = require("./create-tarball");
-createTarball();
---
+++ run_local/runKotiiCli.js
@@ -0,0 +1,7 @@
+//const createTarball = require("./create-tarball");
+const parseScriptArguments = require("./parseScriptArguments");
+const path = require("path");
+
+console.log("WORKING DIR", process.cwd());
+console.log("path_dirname", parseScriptArguments);
+//createTarball();
