9363967f827de2c05451866d5497c3b529381672
ntsakosurprise <mashelesepru@gmail.com>
2023-10-10 09:33:58 +0200

    chore(kotii-cli): updates function to return tarball file name

--- run_local/createTarball.js
+++ run_local/createTarball.js
@@ -1,9 +1,10 @@
 const childProcess = require("child_process");
-module.exports = function (tarballContext = null, tarballName) {
-  let commandToRun = `npm pack ${tarballName}`;
+module.exports = function (tarballContext = null) {
+  let commandToRun = `npm pack`;
   let currentWorkingDirectory = tarballContext ? tarballContext : process.cwd();
-  childProcess.execSync(`${commandToRun}`, {
+  let createdTarPath = childProcess.execSync(`${commandToRun}`, {
     cwd: `${currentWorkingDirectory}`,
     stdio: "inherit",
   });
+  return createdTarPath;
 };
