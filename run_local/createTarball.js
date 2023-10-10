14f667713ca5da01f31a8d271be9f625583fdf54
ntsakosurprise <mashelesepru@gmail.com>
2023-10-10 09:43:29 +0200

    chore(kotii-cli): updates createTarball to return a results to the script instead of the stdio

--- run_local/createTarball.js
+++ run_local/createTarball.js
@@ -2,9 +2,11 @@ const childProcess = require("child_process");
 module.exports = function (tarballContext = null) {
   let commandToRun = `npm pack`;
   let currentWorkingDirectory = tarballContext ? tarballContext : process.cwd();
-  let createdTarPath = childProcess.execSync(`${commandToRun}`, {
-    cwd: `${currentWorkingDirectory}`,
-    stdio: "inherit",
-  });
+  let createdTarPath = childProcess
+    .execSync(`${commandToRun}`, {
+      cwd: `${currentWorkingDirectory}`,
+    })
+    .toString()
+    .trim();
   return createdTarPath;
 };
