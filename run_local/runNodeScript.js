28f11d9619453afe715f43b10724cd3f99a86a0d
ntsakosurprise <mashelesepru@gmail.com>
2023-10-11 19:44:30 +0200

    chore(kotii-cli): properly sets scripts args

--- run_local/runNodeScript.js
+++ run_local/runNodeScript.js
@@ -2,9 +2,9 @@ const { execSync } = require("child_process");
 
 const path = require("path");
 module.exports = function (scriptPath, fileToRun, fromContext, options) {
-  let commandToRun = `node ${path.join(scriptPath, fileToRun)} ${
-    options[0][0]
-  }`;
+  let optionsValue = options[0][0] ? options[0][0] : "";
+
+  let commandToRun = `node ${path.join(scriptPath, fileToRun)} ${optionsValue}`;
   console.log("COMMAND TO RUN", commandToRun);
   execSync(`${commandToRun}`, { cwd: fromContext, stdio: "inherit" });
 };
