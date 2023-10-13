23fded72960515da0f04e15d104de10cc0e31f8e
ntsakosurprise <mashelesepru@gmail.com>
2023-10-13 19:38:11 +0200

    chore(kotii-cli): creates kotii app create-app command

--- packages/kotii-cli/plugins/interpreter/methods.js
+++ packages/kotii-cli/plugins/interpreter/methods.js
@@ -31,143 +31,6 @@ methods.handleInterpreterCliInput = async function (data) {
   filtered.shouldDoCommand
     ? self[filtered.commandAlias](filtered, commands.flags)
     : null;
-
-  /*
-   Use arg package to get passed arguments to the cli. 
-   This arg packages take two objects: 1. An object of commands to be checked for availability.
-   2. An object of possible arguments passed to the cli script/ program
-  */
-
-  /**
-   * Passed expected commands will be included in the _ property of the commands object returned by arg
-     Below, we check if the _ property contain any items, and if the first item is not 'cli'.
-	 About the cli argument, we menually set this at the beginning of our cli operations to be used for
-	 some internal configurations for cli applications built with kotii framework.
-   * */
-  self.infoSync("SET USER COMMANDS");
-  self.infoSync(commands._);
-  if (commands._.length > 0 && commands._[0] !== "cli") {
-    // Get passed user commands from arg object
-    let userPassedCommands = commands._;
-    // Get the first item from the commands. This item is considered a command
-    // let firstItemAsCommand = userPassedCommands[0];
-    // console.log("userPassedCommands", userPassedCommands);
-    // console.log("Objects commands", Object.keys(commands));
-
-    // if (Object.keys(commands).length <= 1) {
-    //   let skip = false;
-    //   if (
-    //     firstItemAsCommand === "create-kotii-app" &&
-    //     userPassedCommands.length > 2
-    //   )
-    //     skip = true;
-
-    //   /*
-    //    Only process commands if they are not create-kotii-app and passed commands are less than or equal to 2.
-    //    These are used for help purposes and end with a 'Command' string
-    //    */
-    //   if (!skip) {
-    //     let com = firstItemAsCommand; // Get the first expected command
-    //     /*
-    // 	map the command to any of the keys defined for a given expected command in the app. This was to enable alternative
-    // 	commands to the cli
-    //   */
-    //     if (com === "--help" || com === "-h") com = "help";
-    //     if (com == "--version" || com === "-v") com = "version";
-    //     if (com === "create-kotii-app") {
-    //       com = "createkotiiAppCommand"; // Set this command for create-kotii-app command help
-    //     } else {
-    //       com = `${com}Command`; // Append Command string to evey command
-    //     }
-
-    //     if (self[com]) {
-    //       return self[com]();
-    //     } else {
-    //       console.log("THE PROCESS IS EXITING WITH ERROR CODE", com);
-    //       process.exit(1);
-    //     }
-    //   }
-    // }
-
-    for (let cmd = 0; cmd < userPassedCommands.length; ++cmd) {
-      let commandName = userPassedCommands[cmd];
-      self.logSync("COMMAND NAME");
-      self.logSync(commandName);
-      self.logSync("THE SELF COMMANDS");
-      self.logSync(self.commands);
-      if (contains(self.commands, commandName) && self[commandName]) {
-        console.log("THE APP CONTAINS THE COMMAND", self[commandName]);
-        if (self[commandName]) self[commandName]();
-        stopFurtherExecution = true;
-        break;
-      } else {
-        let newOptions = { ...commands };
-        newOptions.commands = userPassedCommands;
-        delete newOptions._;
-        let i = newOptions.commands.indexOf("cli");
-        if (i > 0) newOptions.commands.splice(i, 1);
-
-        // console.log(
-        //   chalk.yellow(
-        //     figlet.textSync("Welcome to kotii-CLI", {
-        //       horizontalLayout: "full",
-        //     })
-        //   )
-        // );
-
-        self.emit({
-          type: userPassedCommands[cmd],
-          data: {
-            commands: newOptions.commands,
-            callback: self.getFeedback.bind(self),
-          },
-        });
-        break;
-      }
-    }
-  } else {
-    console.log("THE LENGTH IS");
-    /*
-	  Show available commands that are expected to be passed if none of them has been passed to the script.
-	  showAvailableCommands() contains a list of valid commands available to the cli program.
-	
-	*/
-    return self.showAvailableCommands();
-  }
-
-  if (stopFurtherExecution) return;
-
-  // let message = [  {
-
-  // 	name: 'apptype',
-  // 	type: 'list',
-  // 	message: 'What type of kotii app would you like to create?',
-  // 	choices: ['backend/api/web','cli']
-
-  // 	 }
-  // 	]
-
-  // console.log(
-  // 	chalk.yellow(
-  // 	  figlet.textSync('Welcome to kotii-CLI', { horizontalLayout: 'full' })
-  // 	)
-  //   );
-
-  //   self.prompt({message})
-  //   .then((input)=>{
-
-  // 	console.log(input)
-  // 	console.log(chalk.green(
-  // 		'Question successfully answered'
-  // 	  ))
-
-  //   })
-  //   .catch((e)=>{
-
-  // 	console.log(chalk.red('An error occured prompting for input'));
-  // 	process.exit(1)
-
-  //   })
 };
 
 methods.handlePromptUser = function (data) {
@@ -318,11 +181,14 @@ methods.createKotiiAppCommand = function () {
 	${chalk.greenBright("create-kotii-app <options>")}
 
 	  ${chalk.cyan.bold(
-      "-c | --cli"
-    )}    ................... Creates kotii app suitable for building cli apps
+      "-s | --spa"
+    )}    ................... Creates kotii app suitable simple single page applications
 	  ${chalk.cyan.bold(
-      "-w | --web"
-    )}    ................... Creates kotii app suitable for building web pages, apis, and any backend
+      "-m | --mua"
+    )}    ................... Creates kotii app suitable for building multi-page applications
+    ${chalk.cyan.bold(
+      "-ssr | --ssr"
+    )}    ................... Creates kotii app that is universal(server renders components)
 	  ${chalk.cyan.bold(
       "-r | --remote"
     )} ................... Creates a remote repo and an initial commit for your kotii app 
@@ -442,5 +308,172 @@ methods.capitalizeFirstLetter = function (text) {
 
 methods.createApp = function (commandData, flags = []) {
   console.log("COMMAND, OPTIONS, FLAGS", flags);
+  console.log("COMMAND OPTIONS", commandData);
+  const self = this;
+  const { commandOptions, command } = commandData;
+  const help = flags["--help"] ? true : false;
+
+  if (help) return self.createKotiiAppCommand();
+  if (commandOptions.length === 0) {
+    let message = `Command: ${command}, requires app name, please specify the name of your app`;
+    self.commandOptionMissing(message);
+  }
+  let appName = commandOptions[0];
+  self.emit({
+    type: "scaffold-app",
+    data: {
+      command: { appName, commandName: command },
+      callback: self.getFeedback.bind(self),
+    },
+  });
+};
+
+methods.commandOptionMissing = function (message) {
+  const self = this;
+  self.outPut(message);
+};
+
+methods.__old = function () {
+  /*
+   Use arg package to get passed arguments to the cli. 
+   This arg packages take two objects: 1. An object of commands to be checked for availability.
+   2. An object of possible arguments passed to the cli script/ program
+  */
+
+  /**
+   * Passed expected commands will be included in the _ property of the commands object returned by arg
+     Below, we check if the _ property contain any items, and if the first item is not 'cli'.
+	 About the cli argument, we menually set this at the beginning of our cli operations to be used for
+	 some internal configurations for cli applications built with kotii framework.
+   * */
+  const self = this;
+  const pao = self.pao;
+  const contains = pao.pa_contains;
+  let commands = null;
+  let stopFurtherExecution = false;
+  self.infoSync("SET USER COMMANDS");
+  self.infoSync(commands._);
+  if (commands._.length > 0 && commands._[0] !== "cli") {
+    // Get passed user commands from arg object
+    let userPassedCommands = commands._;
+    // Get the first item from the commands. This item is considered a command
+    // let firstItemAsCommand = userPassedCommands[0];
+    // console.log("userPassedCommands", userPassedCommands);
+    // console.log("Objects commands", Object.keys(commands));
+
+    // if (Object.keys(commands).length <= 1) {
+    //   let skip = false;
+    //   if (
+    //     firstItemAsCommand === "create-kotii-app" &&
+    //     userPassedCommands.length > 2
+    //   )
+    //     skip = true;
+
+    //   /*
+    //    Only process commands if they are not create-kotii-app and passed commands are less than or equal to 2.
+    //    These are used for help purposes and end with a 'Command' string
+    //    */
+    //   if (!skip) {
+    //     let com = firstItemAsCommand; // Get the first expected command
+    //     /*
+    // 	map the command to any of the keys defined for a given expected command in the app. This was to enable alternative
+    // 	commands to the cli
+    //   */
+    //     if (com === "--help" || com === "-h") com = "help";
+    //     if (com == "--version" || com === "-v") com = "version";
+    //     if (com === "create-kotii-app") {
+    //       com = "createkotiiAppCommand"; // Set this command for create-kotii-app command help
+    //     } else {
+    //       com = `${com}Command`; // Append Command string to evey command
+    //     }
+
+    //     if (self[com]) {
+    //       return self[com]();
+    //     } else {
+    //       console.log("THE PROCESS IS EXITING WITH ERROR CODE", com);
+    //       process.exit(1);
+    //     }
+    //   }
+    // }
+
+    for (let cmd = 0; cmd < userPassedCommands.length; ++cmd) {
+      let commandName = userPassedCommands[cmd];
+      self.logSync("COMMAND NAME");
+      self.logSync(commandName);
+      self.logSync("THE SELF COMMANDS");
+      self.logSync(self.commands);
+      if (contains(self.commands, commandName) && self[commandName]) {
+        console.log("THE APP CONTAINS THE COMMAND", self[commandName]);
+        if (self[commandName]) self[commandName]();
+        stopFurtherExecution = true;
+        break;
+      } else {
+        let newOptions = { ...commands };
+        newOptions.commands = userPassedCommands;
+        delete newOptions._;
+        let i = newOptions.commands.indexOf("cli");
+        if (i > 0) newOptions.commands.splice(i, 1);
+
+        // console.log(
+        //   chalk.yellow(
+        //     figlet.textSync("Welcome to kotii-CLI", {
+        //       horizontalLayout: "full",
+        //     })
+        //   )
+        // );
+
+        self.emit({
+          type: userPassedCommands[cmd],
+          data: {
+            commands: newOptions.commands,
+            callback: self.getFeedback.bind(self),
+          },
+        });
+        break;
+      }
+    }
+  } else {
+    console.log("THE LENGTH IS");
+    /*
+     Show available commands that are expected to be passed if none of them has been passed to the script.
+     showAvailableCommands() contains a list of valid commands available to the cli program.
+   
+   */
+    return self.showAvailableCommands();
+  }
+
+  if (stopFurtherExecution) return;
+
+  // let message = [  {
+
+  // 	name: 'apptype',
+  // 	type: 'list',
+  // 	message: 'What type of kotii app would you like to create?',
+  // 	choices: ['backend/api/web','cli']
+
+  // 	 }
+  // 	]
+
+  // console.log(
+  // 	chalk.yellow(
+  // 	  figlet.textSync('Welcome to kotii-CLI', { horizontalLayout: 'full' })
+  // 	)
+  //   );
+
+  //   self.prompt({message})
+  //   .then((input)=>{
+
+  // 	console.log(input)
+  // 	console.log(chalk.green(
+  // 		'Question successfully answered'
+  // 	  ))
+
+  //   })
+  //   .catch((e)=>{
+
+  // 	console.log(chalk.red('An error occured prompting for input'));
+  // 	process.exit(1)
+
+  //   })
 };
 module.exports = methods;
