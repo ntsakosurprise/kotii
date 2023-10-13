666f10000b787cc58f259cc876e0e113a03a8c6a
ntsakosurprise <mashelesepru@gmail.com>
2023-10-13 20:10:26 +0200

    chore(kotii-cli): updates create-app command options

--- packages/kotii-cli/plugins/interpreter/commands.js
+++ packages/kotii-cli/plugins/interpreter/commands.js
@@ -18,10 +18,39 @@ module.exports = {
 
       {
         option: "--template",
-
         description: "Sets the template for kotii framework",
         validValues: ["typescript", "javascript"],
         validValuesAliases: ["ts", "js"],
+      },
+      {
+        option: "--packager",
+        description: "Specifies package manager",
+        validValues: ["npm", "pnp", "yarn"],
+        validValuesAliases: ["n", "p", "y"],
+      },
+      {
+        option: "--private",
+        optionAlias: "-p",
+        type: "boolean",
+        description: "Determines if this should be a private repo",
+      },
+      {
+        option: "--public",
+        optionAlias: "-p",
+        type: "boolean",
+        description: "Determines if this should be a public repo",
+      },
+      {
+        option: "--git",
+        optionAlias: "-g",
+        type: "boolean",
+        description: "Determines if this should be initialized with git",
+      },
+      {
+        option: "--remote",
+        optionAlias: "-r",
+        type: "boolean",
+        description: "Determines if there should be a remote repo",
       },
     ],
   },
