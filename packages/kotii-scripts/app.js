077ef81a88e3ce0b630eef0e00cde2d44434415a
ntsakosurprise <mashelesepru@gmail.com>
2023-10-17 04:05:31 +0200

    chore(kotii-scripts): installs webpack middlewares for anzii server

--- packages/kotii-scripts/app.js
+++ packages/kotii-scripts/app.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 process.argv.push("cli");
+process.env.ANZII_CLI_WITH_SERVER = "true";
 const plugins = require("./plugins/index");
-// const an = require("../../../Development/frameworks/anzii/lib/index");
-// an(plugins);
-require("anzii")(plugins);
+const an = require("../../../Development/frameworks/anzii/lib/index");
+an(plugins);
+// require("anzii")(plugins);
 // console.log(anzii)
