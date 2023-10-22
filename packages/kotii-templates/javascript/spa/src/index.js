95b4c90cb550b2314c617d89669045b81df26ea0
ntsakosurprise <mashelesepru@gmail.com>
2023-10-22 11:22:08 +0200

    chore(kotii-scripts): sets to accept hot module replacement

--- packages/kotii-templates/javascript/spa/src/index.js
+++ packages/kotii-templates/javascript/spa/src/index.js
@@ -1,7 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Home } from "./app";
-
+if (module.hot) {
+  module.hot.accept();
+}
 if (typeof window !== "undefined") {
   const root = createRoot(document.getElementById("root"));
   root.render(
