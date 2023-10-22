e78bb659664b2689ab2a8283fe37ba0447fa439d
ntsakosurprise <mashelesepru@gmail.com>
2023-10-22 11:18:12 +0200

    chore(kotii-scripts): updates webpack config

--- packages/kotii-scripts/webpack.config.js
+++ packages/kotii-scripts/webpack.config.js
@@ -1,13 +1,14 @@
 var path = require("path");
 var webpack = require("webpack");
 var nodeExternals = require("webpack-node-externals");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const root = path.resolve(__dirname);
 
 var anziiCli = {
   entry: ["@babel/polyfill", "./app.js"],
   target: "node",
+  stats: "none",
   externals: [
     { express: "commonjs express" },
     nodeExternals({
@@ -20,7 +21,7 @@ var anziiCli = {
     }),
   ],
   output: {
-    path: path.resolve("dist"),
+    path: path.join(__dirname),
     filename: "index.js",
     libraryTarget: "commonjs2",
   },
@@ -41,17 +42,17 @@ var anziiCli = {
   resolve: {
     roots: [root],
   },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          keep_classnames: false,
-          keep_fnames: false,
-          mangle: true,
-        },
-      }),
-    ],
-  },
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       uglifyOptions: {
+  //         keep_classnames: false,
+  //         keep_fnames: false,
+  //         mangle: true,
+  //       },
+  //     }),
+  //   ],
+  // },
   // devtool: "source-map"
 };
 
