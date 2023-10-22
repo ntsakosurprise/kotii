5fb01e33ac05720509edb12231e40c5e4e36dd81
ntsakosurprise <mashelesepru@gmail.com>
2023-10-22 11:17:13 +0200

    chore(kotii-scripts): changes module resolution statements

--- packages/kotii-scripts/app.js
+++ packages/kotii-scripts/app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 process.argv.push("cli");
-process.env.ANZII_CLI_WITH_SERVER = "true";
+
 const plugins = require("./plugins/index");
-const an = require("../../../Development/frameworks/anzii/lib/index");
+import an from "../../../Development/frameworks/anzii/lib/index";
 an(plugins);
 // require("anzii")(plugins);
 // console.log(anzii)
