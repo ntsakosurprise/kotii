ade0364aef0b32c034b62373286ce214f04b6d00
ntsakosurprise <mashelesepru@gmail.com>
2023-10-24 04:41:52 +0200

    chore(kotii-templates): changes file extensions to jsx

--- packages/kotii-templates/javascript/spa/src/components/home.js
+++ packages/kotii-templates/javascript/spa/src/components/home.js
@@ -1,6 +1,8 @@
 import React from "react";
+// import mapis from "./system/maps";
 
 const Home = () => {
+  // console.log("MAPIS", mapis);
   return (
     <div>
       <p>I AM THE HOME ssrBased COMPONENT</p>
