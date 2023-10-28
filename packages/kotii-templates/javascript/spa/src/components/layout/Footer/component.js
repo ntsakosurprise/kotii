b99b5eb4bccb241f055951b9e1bf1618f0398203
ntsakosurprise <mashelesepru@gmail.com>
2023-10-28 23:56:38 +0200

    chore(kotii-templates): implements footer children

--- packages/kotii-templates/javascript/spa/src/components/layout/Footer/component.js
+++ packages/kotii-templates/javascript/spa/src/components/layout/Footer/component.js
@@ -1,10 +1,24 @@
 import React from "react";
 
+import styled from "styled-components";
+import FooterActions from "./footeractions";
+import FooterBrand from "./footerbrand";
+
+const StyledFooter = styled("footer")({
+  marginTop: "auto",
+
+  display: "flex",
+  flexDirection: "column",
+  // justifyItems: "center",
+  // alignItems: "center",
+});
+
 const Footer = () => {
   return (
-    <div>
-      <p>I am the Footer</p>
-    </div>
+    <StyledFooter>
+      <FooterActions />
+      <FooterBrand />
+    </StyledFooter>
   );
 };
 
