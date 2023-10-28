aa1147bc7fba58d313e59ff64b39ee808fc32e70
ntsakosurprise <mashelesepru@gmail.com>
2023-10-29 00:03:37 +0200

    chore(kotii-templates): adds boilerplate for ssr

---
+++ packages/kotii-templates/javascript/ssr/src/components/startup/Root/component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Routes from "../Routes/component.js";
+
+const Root = () => {
+  // console.log("The pages", getPages());
+  return <Routes />;
+};
+
+export default Root;
