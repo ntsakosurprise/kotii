a5e999dac538a57b2ddf39e44c4a88cc491b9b0c
ntsakosurprise <mashelesepru@gmail.com>
2023-10-28 23:55:59 +0200

    chore(kotii-templates): completes footer setup

--- packages/kotii-templates/javascript/spa/src/pages/index.js
+++ packages/kotii-templates/javascript/spa/src/pages/index.js
@@ -1,7 +1,182 @@
 import React from "react";
+import { AiFillFile, AiFillFolder } from "react-icons/ai";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import styled from "styled-components";
+import SVGConnections from "../shared/test";
 
+const Main = styled("div")({
+  display: "flex",
+  flexDirection: "row",
+  marginTop: "35px",
+  justifyContent: "space-between",
+  position: "relative",
+  @media only screen and (max-width: 450px)": {
+    flexDirection: "column",
+  },
+});
+const Hero = styled("div")(() => {
+  return {
+    marginLeft: "10px",
+    width: "50%",
+  };
+});
+
+const HeroText = styled("p")(() => {
+  return {
+    marginLeft: "0",
+    fontSize: "50px",
+    // width: "90%",
+  };
+});
+
+const HeroPath = styled("p")(() => {
+  return {
+    marginLeft: "10px",
+    fontSize: "20px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "left",
+    marginTop: "25px",
+  };
+});
+
+const PathIcon = styled("small")(() => {
+  return {
+    marginLeft: "0px",
+    fontSize: "20px",
+    // width: "90%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  };
+});
+
+const PathText = styled("small")(() => {
+  return {
+    marginLeft: "2px",
+    fontSize: "20px",
+    // width: "90%",
+    color: "#D680FF",
+    fontWeight: "bolder",
+  };
+});
+
+const PathPointer = styled("small")(() => {
+  return {
+    marginLeft: "2px",
+    fontSize: "20px",
+    marginTop: "5px",
+    // width: "90%",
+  };
+});
+
+const Path = () => {
+  return (
+    <div style={{ minWidth: "100px" }}>
+      <HeroPath>
+        <PathIcon>
+          <AiFillFolder color="#00BFA5" />
+          <PathText>kotii-app</PathText>
+          <PathPointer>
+            <FaLongArrowAltRight />
+          </PathPointer>
+        </PathIcon>
+        <PathIcon>
+          <AiFillFolder color="#00BFA5" />
+          <PathText>src</PathText>
+          <PathPointer>
+            <FaLongArrowAltRight />
+          </PathPointer>
+        </PathIcon>
+
+        <PathIcon>
+          <AiFillFolder color="#00BFA5" />
+          <PathText>components</PathText>
+          <PathPointer>
+            <FaLongArrowAltRight />
+          </PathPointer>
+        </PathIcon>
+
+        <PathIcon>
+          <AiFillFolder color="#00BFA5" />
+          <PathText>pages</PathText>
+          <PathPointer>
+            <FaLongArrowAltRight />
+          </PathPointer>
+        </PathIcon>
+
+        <PathIcon>
+          <AiFillFile color="#00BFA5" />
+          <PathText>index.js</PathText>
+        </PathIcon>
+
+        {/* <PathPointer>
+          <FaLongArrowAltRight />
+        </PathPointer> */}
+        {/* <PathIcon>
+          <AiFillFile color="#00BFA5" />
+          <PathText>index.js</PathText>
+        </PathIcon> */}
+      </HeroPath>
+    </div>
+  );
+};
+
+const StyledButton = styled("button")({
+  border: "none",
+  width: "150px",
+  backgroundColor: "inherit",
+  marginTop: "30px",
+  cursor: "pointer",
+  position: "relative",
+});
+
+const ButtonFrontCard = styled("small")({
+  backgroundColor: "#00BFA5",
+  width: "100%",
+  height: "50px",
+  display: "flex",
+  position: "relative",
+  zIndex: 5,
+  justifyContent: "center",
+  alignItems: "center",
+  color: "white",
+  fontWeight: "bolder",
+  lineHeight: "3px",
+});
+const ButtonBackCard = styled("small")({
+  backgroundColor: "#F7C02B",
+  width: "100%",
+  height: "50px",
+  display: "flex",
+  position: "absolute",
+});
+
+const SVG = styled("div")({
+  display: "flex",
+  width: "50%",
+  position: "relative",
+});
 const Index = () => {
-  return <p>Im the index page</p>;
+  return (
+    <Main>
+      <Hero>
+        <HeroText>
+          Edit, save, and see your changes reflected in real-time. Get started
+          by going to:
+        </HeroText>
+        <Path />
+        <StyledButton>
+          <ButtonBackCard />
+          <ButtonFrontCard>Learn More </ButtonFrontCard>
+        </StyledButton>
+        {/* <img src={connectionsSvg} width={50} alt="connections svg" /> */}
+      </Hero>
+      <SVG>
+        <SVGConnections />
+      </SVG>
+    </Main>
+  );
 };
 
 export default Index;
