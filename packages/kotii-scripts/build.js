201c78ac3feb246755390ce1c990f5a4b7ce2d19
ntsakosurprise <mashelesepru@gmail.com>
2023-11-13 15:48:53 +0200

    chore(kotii-scripts): updates router compnents with jsx

--- packages/kotii-scripts/build.js
+++ packages/kotii-scripts/build.js
@@ -1,114 +1,62 @@
 import React from "react";
 import { BrowserRouter, Switch as ReactRoutes } from "react-router-dom";
 import Public from "./public.js";
-const mapsOfFiles = [
-  {
-    path: "/test",
-    component: function Test() {
-      return /*#__PURE__*/ _react.default.createElement("p", null, "TEST Page");
-    },
-  },
-  {
-    path: "/",
-    component: function Index() {
-      return /*#__PURE__*/ _react.default.createElement(
-        Hero,
-        null,
-        /*#__PURE__*/ _react.default.createElement(
-          HeroText,
-          null,
-          "Focus on your idea, Forget about configurations"
-        ),
-        ";"
-      );
-    },
-  },
-  {
-    path: "/faqs",
-    component: function Faqs() {
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the FAQS page"
-      );
-    },
-  },
-  {
-    path: "/contact-us",
-    component: function ContactUs() {
-      console.log("THE ABOUT PAGE");
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the ABOUT page"
-      );
-    },
-  },
-  {
-    path: "/about",
-    component: function About() {
-      console.log("THE ABOUT PAGE");
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the ABOUT page"
-      );
-    },
-  },
-  {
-    path: "/todo",
-    component: function Todo() {
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the TODO page"
-      );
-    },
-  },
-  {
-    path: "/posts",
-    component: function Posts() {
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the Posts page"
-      );
-    },
-  },
-  {
-    path: "/posts/:slug",
-    component: function AnythingSlug() {
-      return /*#__PURE__*/ _react.default.createElement(
-        "p",
-        null,
-        "Im the AnythingSlug page"
-      );
-    },
-  },
-];
+const mapsOfFiles = [{
+  path: "/test",
+  component: function Test() {
+    return <p>TEST Page</p>;
+  }
+}, {
+  path: "/",
+  component: function Index() {
+    return <p>Focus on your idea, Forget about configurations</p>;
+  }
+}, {
+  path: "/faqs",
+  component: function Faqs() {
+    return <p>Im the FAQS page</p>;
+  }
+}, {
+  path: "/contact-us",
+  component: function ContactUs() {
+    console.log("THE ABOUT PAGE");
+    return <p>Im the ABOUT page</p>;
+  }
+}, {
+  path: "/about",
+  component: function About() {
+    console.log("THE ABOUT PAGE");
+    return <p>Im the ABOUT page</p>;
+  }
+}, {
+  path: "/todo",
+  component: function Todo() {
+    return <p>Im the TODO page</p>;
+  }
+}, {
+  path: "/posts",
+  component: function Posts() {
+    return <p>Im the Posts page</p>;
+  }
+}, {
+  path: "/posts/:slug",
+  component: function AnythingSlug() {
+    return <p>Im the AnythingSlug page</p>;
+  }
+}];
 const name = "my name is my name";
 const surname = "Mashele";
 export { name, surname, mapsOfFiles };
 
 //import Public from "../Public/component.js"
 
-const Routes = (props) => {
-  return (
-    <BrowserRouter>
+const Routes = props => {
+  return <BrowserRouter>
       <ReactRoutes>
         {mapsOfFiles.map((r, index) => {
-          return (
-            <Public
-              {...props}
-              exact
-              path={r.path}
-              component={r.component}
-              key={index}
-            />
-          );
-        })}
+        return <Public {...props} exact path={r.path} component={r.component} key={index} />;
+      })}
       </ReactRoutes>
-    </BrowserRouter>
-  );
+    </BrowserRouter>;
 };
 export default Routes;
