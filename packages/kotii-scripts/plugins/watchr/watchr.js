da2fb8b29c930ff7bbd6aa477b76bf686774b3d3
ntsakosurprise <mashelesepru@gmail.com>
2023-11-19 16:58:27 +0200

    chore(kotii-scripts):  intergrates watcher with chokidar

--- packages/kotii-scripts/plugins/watchr/watchr.js
+++ packages/kotii-scripts/plugins/watchr/watchr.js
@@ -1,7 +1,9 @@
+import chokidar from "chokidar";
 import methods from "./methods.js";
 class Watchr {
   constructor(pao) {
     this.pao = pao;
+    this.chokidar = chokidar;
     this.init = methods.init;
     this.handleWatch = methods.handleWatch;
   }
