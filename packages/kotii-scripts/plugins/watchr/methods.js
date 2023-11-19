0780228231ada879a6b47832b8641e9f93e3ba09
ntsakosurprise <mashelesepru@gmail.com>
2023-11-19 16:59:37 +0200

    chore(kotii-scripts):  creates watcher watching events

--- packages/kotii-scripts/plugins/watchr/methods.js
+++ packages/kotii-scripts/plugins/watchr/methods.js
@@ -3,24 +3,45 @@ methods.init = function () {
   // console.log('Bitbucket has been initialised')
 
   this.listens({
-    init: this.handleWatch.bind(this),
+    "watch-target": this.handleWatch.bind(this),
   });
 };
 methods.handleWatch = function (data) {
-  console.log("THE DATA OF Init SCRIPTS", data);
-  data.callback({ message: "Init plugin successfully called" });
-  return;
+  // console.log("THE DATA OF Init SCRIPTS", data);
+  // data.callback({ message: "Init plugin successfully called" });
+  // return;
 };
-methods.namespace = function (data) {
+methods.watchFiles = function (data) {
   const self = this;
-  const clientOptions = { auth: data.creds };
-  const bitbucket = new Bitbucket(clientOptions);
-  return bitbucket;
+  const { callback, payload } = data;
+  const {
+    watched,
+    persistent = false,
+    ignored = null,
+    events = null,
+  } = payload;
+
+  const watcher = self.chokidar.watch(watched, {
+    persistent,
+    ignored,
+    // ignored: /(^|[\/\\])\../, // ignore dotfiles
+    // persistent: true
+  });
+
+  if (!events)
+    return callback({
+      status: true,
+      eventsAttached: false,
+      message: "Files are successfully watched",
+    });
+  const { add = () => {}, change = () => {} } = events;
+  watcher.on("add", add);
+  watcher.on("change", change);
+  callback({
+    status: true,
+    eventsAttached: true,
+    message: "Files are successfully watched and events have been attached",
+  });
 };
-methods.api = function (data) {
-  const self = this;
-  const clientOptions = { auth: data.token };
-  const bitbucket = new Bitbucket(clientOptions);
-  return bitbucket;
-};
+
 export default methods;
