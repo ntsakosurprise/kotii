e7b4b64110d413d6fe3bfab1e85bcec43d4e5bfd
ntsakosurprise <mashelesepru@gmail.com>
2023-11-19 15:29:33 +0200

    chore(kotii-scripts): configure cache dependents

--- packages/kotii-scripts/plugins/cachr/cachr.js
+++ packages/kotii-scripts/plugins/cachr/cachr.js
@@ -1,9 +1,15 @@
 import methods from "./methods.js";
+import NodeCache from ("node-cache");
+
 class Cachr {
   constructor(pao) {
     this.pao = pao;
     this.init = methods.init;
+    this.NodeCache = NodeCache
+    this.cache = null
     this.handleCache = methods.handleCache;
+    this.handleGetData = methods.handleGetData
+    this.doCache = methods.doCache
   }
 }
 export default Cachr;
