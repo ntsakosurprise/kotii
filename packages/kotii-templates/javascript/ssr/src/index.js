a2a571a0cc7e366ecf37801bb7960288ef6e47f7
ntsakosurprise <mashelesepru@gmail.com>
2023-11-24 22:02:47 +0200

    chore(kotii-scripts):  updates index.js to reference app from kotii-scripts

--- packages/kotii-templates/javascript/ssr/src/index.js
+++ packages/kotii-templates/javascript/ssr/src/index.js
@@ -1,14 +1,15 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app.js";
+import App from "kotii-scripts";
+// import React, { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+App();
 // if (module.hot) {
 //   module.hot.accept();
 // }
-if (typeof window !== "undefined") {
-  const root = createRoot(document.getElementById("root"));
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+// if (typeof window !== "undefined") {
+//   const root = createRoot(document.getElementById("root"));
+//   root.render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   );
+// }
