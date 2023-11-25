aaed7383e902764573137aa8f37d8b2ef9e0fc81
ntsakosurprise <mashelesepru@gmail.com>
2023-11-25 10:10:00 +0200

    chore(kotii-scripts):  updates app_.js to cater for app wrapper and app layout

--- packages/kotii-scripts/app_.js
+++ packages/kotii-scripts/app_.js
@@ -4,14 +4,42 @@ import Routes from "./build.js";
 // if (module.hot) {
 //   module.hot.accept();
 // }
-const App = () => {
-  if (typeof window !== "undefined") {
-    const root = createRoot(document.getElementById("root"));
-    root.render(
-      <StrictMode>
-        <Routes />
-      </StrictMode>
-    );
+const App = (appWrapper = null, layout = null) => {
+  if (appWrapper || layout) {
+    if (typeof window !== "undefined") {
+      const AppWrapper = appWrapper;
+      //   const Layout = props.layout;
+      if (appWrapper && layout) {
+        const Layout = layout;
+        const root = createRoot(document.getElementById("root"));
+        root.render(
+          <StrictMode>
+            <AppWrapper>
+              <Routes layout={Layout} />
+            </AppWrapper>
+          </StrictMode>
+        );
+      } else {
+        const Layout = layout;
+        const root = createRoot(document.getElementById("root"));
+        root.render(
+          <StrictMode>
+            <AppWrapper>
+              <Routes layout={Layout} />
+            </AppWrapper>
+          </StrictMode>
+        );
+      }
+    }
+  } else {
+    if (typeof window !== "undefined") {
+      const root = createRoot(document.getElementById("root"));
+      root.render(
+        <StrictMode>
+          <Routes />
+        </StrictMode>
+      );
+    }
   }
 };
 
