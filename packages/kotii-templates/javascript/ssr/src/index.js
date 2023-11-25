ecaadb2aa210cdb0759dfee23cfee5281761aed9
ntsakosurprise <mashelesepru@gmail.com>
2023-11-25 10:14:01 +0200

    chore(kotii-templates):  passes app root and app layout to kotii App

--- packages/kotii-templates/javascript/ssr/src/index.js
+++ packages/kotii-templates/javascript/ssr/src/index.js
@@ -1,15 +1,5 @@
 import App from "kotii-scripts";
-// import React, { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-App();
-// if (module.hot) {
-//   module.hot.accept();
-// }
-// if (typeof window !== "undefined") {
-//   const root = createRoot(document.getElementById("root"));
-//   root.render(
-//     <StrictMode>
-//       <App />
-//     </StrictMode>
-//   );
-// }
+// import AppRoot from "";
+import { Layout, Root } from "Startup";
+
+App(Root, Layout);
