c5b3223bafed9fc386e7abee4b40f40f5e0dca5a
ntsakosurprise <mashelesepru@gmail.com>
2023-11-25 12:08:17 +0200

    chore(kotii-templates):  removes imports from kotii-scripts

--- packages/kotii-templates/javascript/ssr/src/pages/privacy.js
+++ packages/kotii-templates/javascript/ssr/src/pages/privacy.js
@@ -1,7 +1,4 @@
-import Routes, { name } from "kotii-scripts";
 const Test = function () {
-  console.log("KOTTI IMPORTS ", name);
-  console.log("THE KOTII-ROUTES", Routes);
   //console.log(doingTheMost());
   return console.log("I AM KOTII TEST FUNCTION");
 };
