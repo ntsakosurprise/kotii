63483aad87a810068da7ece018704f4411f4e387
ntsakosurprise <mashelesepru@gmail.com>
2023-11-25 12:07:43 +0200

    chore(kotii-templates):  uses hooks for test in Faqs

--- packages/kotii-templates/javascript/ssr/src/pages/faqs.js
+++ packages/kotii-templates/javascript/ssr/src/pages/faqs.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Faqs = () => {
-  return <p>Im the FAQS page</p>;
+  const [test, setTestState] = useState(`It's a test state`);
+  useEffect(() => {
+    console.log("THE COMPONENT HAS LOADED", test);
+  }, []);
+  useEffect(() => {
+    console.log("THE COMPONENT STATE HAS CHANGED TO:", test);
+  }, [test]);
+  return (
+    <p>
+      <button
+        onClick={() => {
+          setTestState("THE NEW STATE IS TO");
+        }}
+      >
+        FAQS Button
+      </button>
+    </p>
+  );
 };
 
 export default Faqs;
