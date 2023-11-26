1f64d14e516aaefdd76bcad977898874d2afff1b
ntsakosurprise <mashelesepru@gmail.com>
2023-11-26 16:19:50 +0200

    chore(kotii-scripts):  adds config for testing endpoints

---
+++ packages/kotii-scripts/.config.js
@@ -0,0 +1,18 @@
+export const domain = [{ name: "static", set: "public" }];
+export const router = [
+  {
+    path: "/greeting/:name/:surname",
+    alias: "hello",
+    method: "GET",
+    type: "public",
+  },
+];
+
+export const server = "server";
+export default {
+  domain,
+  router,
+  logger,
+  cluster,
+  server,
+};
