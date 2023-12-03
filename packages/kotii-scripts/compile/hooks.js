07515157d780eeac34c0842e5293825073c97f5d
ntsakosurprise <mashelesepru@gmail.com>
2023-12-03 13:58:03 +0200

    chore(kotii-scripts): updates load hook to replace common js module with esm for styled-components as a fix to styled function being undefined error

--- packages/kotii-scripts/compile/hooks.js
+++ packages/kotii-scripts/compile/hooks.js
@@ -1,31 +1,52 @@
 import babel from "@babel/core";
 import fs from "fs";
+import path from "path";
 let whiteListedUrls = [
   "file:///Users/surprisemashele/Documents/kotii/packages/kotii-scripts/plugins/react/methods.js",
   "file:///Users/surprisemashele/Documents/kotii/packages/kotii-scripts/build.js",
   "file:///Users/surprisemashele/Documents/kotii/packages/kotii-scripts/public.js",
 ];
-let pagesURL =
-  "file:///Users/surprisemashele/Documents/kotii/packages/kotii-templates/javascript/ssr/src/pages/";
+const styledComponentsUrl =
+  "file:///Users/surprisemashele/Documents/kotii/node_modules/styled-components/dist/styled-components.cjs.js";
+const styledComponentESMUrm =
+  "file:///Users/surprisemashele/Documents/kotii/node_modules/styled-components/dist/styled-components.esm.js";
+// let pagesURL =
+//   "file:///Users/surprisemashele/Documents/kotii/packages/kotii-templates/javascript/ssr/src/pages/";
 let extJsx = ".jsx";
 export async function load(url, context, nextLoad) {
   // Take a resolved URL and return the source code to be evaluated.
   const { format } = context;
+  const fileExtension = path.extname(url);
   console.log("LOAD HOOK FORMAT", format);
   console.log("LOAD HOOK FORMAT URL", url);
+  console.log("LOAD EXTENSION NAME", fileExtension);
+  if (format === "commonjs" && url === styledComponentsUrl) {
+    let cutCode = fs.readFileSync(new URL(styledComponentESMUrm).pathname, {
+      encoding: "utf-8",
+    });
+    console.log("THE CUTCODE", cutCode);
+    return {
+      format: "module",
+      shortCircuit: true,
+      source: cutCode,
+    };
+  }
 
-  if (
-    whiteListedUrls.indexOf(url) >= 0 ||
-    url.indexOf(extJsx) >= 0 ||
-    url.indexOf(pagesURL) >= 0
-  ) {
+  if (whiteListedUrls.indexOf(url) >= 0 || fileExtension === extJsx) {
     console.log("JSX METHODS FILE", url);
     console.log("THE CONTEXT", context);
 
-    const source =
-      url.indexOf(extJsx) < 0
-        ? await nextLoad(url, { ...context, format })
-        : fs.readFileSync(new URL(url).pathname, { encoding: "utf-8" });
+    let source = null;
+    if (fileExtension === extJsx) {
+      console.log("PROCESSING FOR JSX EXTENSION");
+
+      source = fs.readFileSync(new URL(url).pathname, { encoding: "utf-8" });
+      console.log("The Resulting Source", source);
+    } else {
+      console.log("PROCESSING JSX WITHOUT EXTENSION");
+      source = await nextLoad(url, { ...context, format });
+    }
+
     console.log("THE RAW SOURCE", source);
     let rawSource = typeof source === "string" ? source : source.source;
     let options = {};
@@ -50,29 +71,12 @@ export async function load(url, context, nextLoad) {
 }
 
 // let imports = {};
-// export async function resolve(specifier, context, nextResolve) {
-//   const { parentURL = null } = context;
-//   // console.log("RESOLVE specifier", specifier);
-//   // console.log("RESOLVE context", context);
-//   // console.log("RESOLVE nextResolve", nextResolve);
-//   // const { parentUrl } = context;
-//   if (
-//     specifier === "./react/reactview.js" ||
-//     specifier.indexOf("react/methods.js" >= 0)
-//   ) {
-//     console.log("THE SPECIFIER", specifier);
-//     // context["fileName"] = specifier;
-//     // context["parentFile"] = pare;
-//     return {
-//       format: "module",
-//       url: parentURL
-//         ? new URL(specifier, parentURL).href
-//         : new URL(specifier).href,
-//       custom: { specifier: specifier, custom: "cust" },
-//       context: { ...context },
-//       shortCircuit: true,
-//     };
-//   }
-//   return nextResolve(specifier);
-//   // Take an `import` or `require` specifier and resolve it to a URL.
-// }
+export async function resolve(specifier, context, nextResolve) {
+  const { parentURL = null } = context;
+  console.log("RESOLVE specifier", specifier);
+  console.log("RESOLVE context", context);
+  console.log("RESOLVE nextResolve", nextResolve);
+  const { parentUrl } = context;
+  return nextResolve(specifier);
+  // Take an `import` or `require` specifier and resolve it to a URL.
+}
