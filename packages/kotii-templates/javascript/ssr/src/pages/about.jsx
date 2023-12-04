ace1bc7436be88b1e95f295afe42b431b1d79983
ntsakosurprise <mashelesepru@gmail.com>
2023-12-04 04:44:51 +0200

    chore(kotii-scripts): updates component for testing live-reload

--- packages/kotii-templates/javascript/ssr/src/pages/about.jsx
+++ packages/kotii-templates/javascript/ssr/src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   console.log("THE ABOUT PAGE");
-  return <p>Im the ABOUT page</p>;
+  return <p>Im the ABOUT pageZ</p>;
 };
 
 export default About;
