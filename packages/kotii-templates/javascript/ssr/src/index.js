81d6e66bcb6bcfca4dfa8239046fbc0cffffaef2
ntsakosurprise <mashelesepru@gmail.com>
2023-12-08 02:09:27 +0200

    chore(kotii-scripts): implements hot module for the browser to accept updates from the server

--- packages/kotii-templates/javascript/ssr/src/index.js
+++ packages/kotii-templates/javascript/ssr/src/index.js
@@ -1,3 +1,7 @@
+if (import.meta.webpackHot) {
+  console.log("THE META.HOT");
+  import.meta.webpackHot.accept();
+}
 import App from "kotii-scripts";
 // import AppRoot from "";
 import { Layout, Root } from "Startup";
