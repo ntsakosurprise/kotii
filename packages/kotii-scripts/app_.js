b4302a2664033962fe3e20cb6a68656bff9e0c27
ntsakosurprise <mashelesepru@gmail.com>
2023-12-08 07:02:46 +0200

    chore(kotii-scripts): updates app_.js for hydration on the client

--- packages/kotii-scripts/app_.js
+++ packages/kotii-scripts/app_.js
@@ -1,9 +1,48 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import { hydrateRoot } from "react-dom/client";
 import Routes from "./build.js";
 // if (module.hot) {
 //   module.hot.accept();
 // }
+// const App = (appWrapper = null, layout = null) => {
+//   if (appWrapper || layout) {
+//     if (typeof window !== "undefined") {
+//       const AppWrapper = appWrapper;
+//       //   const Layout = props.layout;
+//       if (appWrapper && layout) {
+//         const Layout = layout;
+//         const root = createRoot(document.getElementById("root"));
+//         root.render(
+//           <StrictMode>
+//             <AppWrapper>
+//               <Routes layout={Layout} />
+//             </AppWrapper>
+//           </StrictMode>
+//         );
+//       } else {
+//         const Layout = layout;
+//         const root = createRoot(document.getElementById("root"));
+//         root.render(
+//           <StrictMode>
+//             <AppWrapper>
+//               <Routes layout={Layout} />
+//             </AppWrapper>
+//           </StrictMode>
+//         );
+//       }
+//     }
+//   } else {
+//     if (typeof window !== "undefined") {
+//       const root = createRoot(document.getElementById("root"));
+//       root.render(
+//         <StrictMode>
+//           <Routes />
+//         </StrictMode>
+//       );
+//     }
+//   }
+// };
+
 const App = (appWrapper = null, layout = null) => {
   if (appWrapper || layout) {
     if (typeof window !== "undefined") {
@@ -11,34 +50,32 @@ const App = (appWrapper = null, layout = null) => {
       //   const Layout = props.layout;
       if (appWrapper && layout) {
         const Layout = layout;
-        const root = createRoot(document.getElementById("root"));
-        root.render(
-          <StrictMode>
-            <AppWrapper>
-              <Routes layout={Layout} />
-            </AppWrapper>
-          </StrictMode>
+        hydrateRoot(
+          document.getElementById("root"),
+          <AppWrapper>
+            <Routes layout={Layout} />
+          </AppWrapper>
         );
       } else {
         const Layout = layout;
-        const root = createRoot(document.getElementById("root"));
-        root.render(
-          <StrictMode>
-            <AppWrapper>
-              <Routes layout={Layout} />
-            </AppWrapper>
-          </StrictMode>
+        hydrateRoot(
+          document.getElementById("root"),
+          <AppWrapper>
+            <Routes layout={Layout} />
+          </AppWrapper>
         );
+        // root.render(
+        //   <StrictMode>
+        //     <AppWrapper>
+        //       <Routes layout={Layout} />
+        //     </AppWrapper>
+        //   </StrictMode>
+        // );
       }
     }
   } else {
     if (typeof window !== "undefined") {
-      const root = createRoot(document.getElementById("root"));
-      root.render(
-        <StrictMode>
-          <Routes />
-        </StrictMode>
-      );
+      hydrateRoot(document.getElementById("root"), <Routes />);
     }
   }
 };
