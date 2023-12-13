98c2e8a61e928679498a1736b179a87ab524d0ec
ntsakosurprise <mashelesepru@gmail.com>
2023-12-13 21:48:30 +0200

    chore(kotii-scripts): exports a function that lets the consumer creates a store

--- packages/kotii-scripts/app_redux.js
+++ packages/kotii-scripts/app_redux.js
@@ -2,6 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { reducers } from "/Users/surprisemashele/Documents/kotii/packages/kotii-templates/javascript/ssr/src/store/index.js";
 
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
-
-export { store };
+export default (initialState = {}) => {
+  console.log("THE PRELOADED STATE>>>", initialState);
+  return createStore(
+    combineReducers(reducers),
+    initialState,
+    applyMiddleware(thunk)
+  );
+};
