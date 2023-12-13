63e935f3be00e9bbd40bd2e3c56069c5a6c004f6
ntsakosurprise <mashelesepru@gmail.com>
2023-12-13 21:47:48 +0200

    chore(kotii-scripts): set app file to import createReduxStore function

--- packages/kotii-scripts/app_.js
+++ packages/kotii-scripts/app_.js
@@ -1,52 +1,11 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app_redux.js";
+import createReduxStore from "./app_redux.js";
 import ClientRoutes, { RoutesAsServerRoutes } from "./build.js";
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-// const App = (appWrapper = null, layout = null) => {
-//   if (appWrapper || layout) {
-//     if (typeof window !== "undefined") {
-//       const AppWrapper = appWrapper;
-//       //   const Layout = props.layout;
-//       if (appWrapper && layout) {
-//         const Layout = layout;
-//         const root = createRoot(document.getElementById("root"));
-//         root.render(
-//           <StrictMode>
-//             <AppWrapper>
-//               <Routes layout={Layout} />
-//             </AppWrapper>
-//           </StrictMode>
-//         );
-//       } else {
-//         const Layout = layout;
-//         const root = createRoot(document.getElementById("root"));
-//         root.render(
-//           <StrictMode>
-//             <AppWrapper>
-//               <Routes layout={Layout} />
-//             </AppWrapper>
-//           </StrictMode>
-//         );
-//       }
-//     }
-//   } else {
-//     if (typeof window !== "undefined") {
-//       const root = createRoot(document.getElementById("root"));
-//       root.render(
-//         <StrictMode>
-//           <Routes />
-//         </StrictMode>
-//       );
-//     }
-//   }
-// };
-
 const App = (appWrapper = null, layout = null) => {
+  const store = createReduxStore(window.__PRELOADED_STATE__);
   if (appWrapper || layout) {
     if (typeof window !== "undefined") {
       const AppWrapper = appWrapper;
@@ -87,7 +46,12 @@ const App = (appWrapper = null, layout = null) => {
   }
 };
 
-const ServerApp = (appWrapper = null, layout = null) => {
+const ServerApp = (
+  appWrapper = null,
+  layout = null,
+  storeFromSource = null
+) => {
+  const store = !storeFromSource ? createReduxStore() : storeFromSource;
   if (appWrapper || layout) {
     if (appWrapper && layout) {
       const AppWrapper = appWrapper;
