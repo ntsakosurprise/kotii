85b8e505d59038ce99df895f694bf4b2565722e5
ntsakosurprise <mashelesepru@gmail.com>
2023-12-18 00:02:46 +0200

    chore(kotii-scripts): updates buildsc plugin with doStaticSiteGeneration method

--- packages/kotii-scripts/plugins/buildsc/build.js
+++ packages/kotii-scripts/plugins/buildsc/build.js
@@ -1,9 +1,10 @@
 import methods from "./methods.js";
 class Build {
-    constructor(pao) {
-        this.pao = pao;
-        this.init = methods.init;
-        this.handleBuildScript = methods.handleBuildScript;
-    }
+  constructor(pao) {
+    this.pao = pao;
+    this.init = methods.init;
+    this.handleBuildScript = methods.handleBuildScript;
+    this.doStaticSiteGeneration = methods.doStaticSiteGeneration;
+  }
 }
 export default Build;
--- packages/kotii-scripts/plugins/buildsc/methods.js
+++ packages/kotii-scripts/plugins/buildsc/methods.js
@@ -1,24 +1,33 @@
 const methods = {};
 methods.init = function () {
-    this.listens({
-        build: this.handleBuildScript.bind(this),
-    });
+  this.listens({
+    build: this.handleBuildScript.bind(this),
+  });
 };
 methods.handleBuildScript = function (data) {
-    console.log("THE DATA OF Build SCRIPTS", data);
-    data.callback({ message: "Build plugin successfully called" });
-    return;
+  console.log("THE DATA OF Build SCRIPTS", data);
+
+  //   data.callback({ message: "Build plugin successfully called" });
+  const self = this;
+  self.doStaticSiteGeneration({ callback: data.callback });
+  return;
 };
-methods.namespace = function (data) {
-    const self = this;
-    const clientOptions = { auth: data.creds };
-    const bitbucket = new Bitbucket(clientOptions);
-    return bitbucket;
+methods.doStaticSiteGeneration = function (data) {
+  const self = this;
+  self.emit({
+    type: "generate-static-content",
+    data: {
+      callback: (gotValue) => {
+        console.log("STATIC GENERATION IS COMPLETED", gotValue);
+        data.callback({ message: "Build plugin successfully called" });
+      },
+    },
+  });
 };
 methods.api = function (data) {
-    const self = this;
-    const clientOptions = { auth: data.token };
-    const bitbucket = new Bitbucket(clientOptions);
-    return bitbucket;
+  const self = this;
+  const clientOptions = { auth: data.token };
+  const bitbucket = new Bitbucket(clientOptions);
+  return bitbucket;
 };
 export default methods;
