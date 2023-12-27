7d9249702efdcba34b0844a84eef949698d9889a
ntsakosurprise <mashelesepru@gmail.com>
2023-12-27 11:31:46 +0200

    test(root): update jest root config
    This is a temp implementation to run tests from one command. We still
    have to do more research on Jest configurations for monorepos.

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,9 @@
-const kotiiMardownPkg = require("./packages/kotii-markdown/package.json");
-const kotiiUIPkg = require("./packages/kotii-ui/package.json");
+//const kotiiMardownPkg = require("./packages/kotii-markdown/package.json");
+// const kotiiUIPkg = require("./packages/kotii-ui/package.json");
 module.exports = {
   verbose: true,
   projects: [
-    {
-      //   preset: "ts-jest/presets/js-with-ts",
-      //   testEnvironment: "jsdom",
-      //   setupFilesAfterEnv: ["./jest.setup.js"],
-      displayName: kotiiMardownPkg.name,
-      testMatch: [
-        "<rootDir>/packages/kotii-markdown/**/?(*.)+(spec|test).[jt]s?(x)",
-      ],
-    },
-    {
-      preset: "ts-jest/presets/js-with-ts",
-      testEnvironment: "jsdom",
-      //   setupFilesAfterEnv: ["./jest.setup.js"],
-      displayName: kotiiUIPkg.name,
-      testMatch: ["<rootDir>/packages/kotii-ui/**/?(*.)+(spec|test).[jt]s?(x)"],
-    },
+    "<rootDir>/packages/kotii-markdown/jest.config.js",
+    "<rootDir>/packages/kotii-ui/jest.config.ts",
   ],
 };
