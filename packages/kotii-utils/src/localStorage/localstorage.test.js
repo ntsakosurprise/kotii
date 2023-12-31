090afb6c8f7dce97f146b8a80918bc5cb2af78b1
ntsakosurprise <mashelesepru@gmail.com>
2023-12-31 10:05:00 +0200

    fix(kotii-utils): create tests for localStorage utils

--- packages/kotii-utils/src/localStorage/localstorage.test.js
+++ packages/kotii-utils/src/localStorage/localstorage.test.js
@@ -1,29 +1,24 @@
-import {
-  getFromStorage,
-  removeFromStorage,
-  setInStorage,
-} from "./localStorage";
+import { getFromStorage, removeFromStorage, setInStorage } from "./index.js";
 
 describe("Test localStorage methods", () => {
   describe("Test LocalStorage.setInStorage", () => {
     test("Should set/store a given value in storage", () => {
       expect(
         setInStorage(
-          JSON.stringify({
-            profile: JSON.stringify({ name: "Ntsako", surname: "Mashele" }),
-          })
+          "profile",
+          JSON.stringify({ name: "Ntsako", surname: "Mashele" })
         )
       ).toBeTruthy();
     });
   });
   describe("Test LocalStorage.getFromStorage", () => {
-    test("Should set/store a given value in storage", () => {
+    test("Should get a set key's value from storage", () => {
       expect(getFromStorage("profile")).toBeTruthy();
     });
   });
 
   describe("Test LocalStorage.removeFromStorage", () => {
-    test("Should set/store a given value in storage", () => {
+    test("Should remove a given key from storage", () => {
       expect(removeFromStorage("profile")).toBeTruthy();
     });
   });
