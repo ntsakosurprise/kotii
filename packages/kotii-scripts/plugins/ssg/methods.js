f45728311704b5fd25ce95a67fe3be6cb3e592e9
ntsakosurprise <mashelesepru@gmail.com>
2024-05-26 04:26:03 +0200

    chore(kotii-scripts): remove dead code from ssg

--- packages/kotii-scripts/plugins/ssg/methods.js
+++ packages/kotii-scripts/plugins/ssg/methods.js
@@ -38,16 +38,5 @@ methods.getWebPackConfig = function (dataToConfig, setCall) {
     },
   });
 };
-methods.namespace = function (data) {
-  const self = this;
-  const clientOptions = { auth: data.creds };
-  const bitbucket = new Bitbucket(clientOptions);
-  return bitbucket;
-};
-methods.api = function (data) {
-  const self = this;
-  const clientOptions = { auth: data.token };
-  const bitbucket = new Bitbucket(clientOptions);
-  return bitbucket;
-};
+
 export default methods;
