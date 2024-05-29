f35c0d445ab644f1fbfdfe5133c292f51d1328f1
ntsakosurprise <mashelesepru@gmail.com>
2024-05-29 04:18:38 +0200

    chore(kotii-scripts): load files dynamically
    This is to help kotii's set up of a project be as fitting as possible

--- packages/kotii-scripts/app_.js
+++ packages/kotii-scripts/app_.js
@@ -3,48 +3,78 @@ import { hydrateRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import createReduxStore from "./app_redux.js";
 // import ClientRoutes, { RoutesAsServerRoutes } from "./build.js";
-import { ClientRoutes, RoutesAsServerRoutes } from "./build.js";
+import { RoutesAsServerRoutes } from "./build.js";
 
 const App = (appWrapper = null, layout = null) => {
-  const store = createReduxStore(window.__PRELOADED_STATE__);
-  if (appWrapper || layout) {
-    if (typeof window !== "undefined") {
-      const AppWrapper = appWrapper;
-      //   const Layout = props.layout;
-      if (appWrapper && layout) {
-        const Layout = layout;
-        hydrateRoot(
-          document.getElementById("root"),
-          <Provider store={store}>
-            <AppWrapper>
-              <ClientRoutes layout={Layout} />
-            </AppWrapper>
-          </Provider>
-        );
+  import("./manifest.js").then((importedManifest) => {
+    let meta = importedManifest.meta;
+    import("./build.js").then((importedBuild) => {
+      const ClientRoutes = importedBuild.ClientRoutes;
+      const store = createReduxStore(window.__PRELOADED_STATE__);
+
+      if (appWrapper || layout) {
+        if (typeof window !== "undefined") {
+          const AppWrapper = appWrapper;
+          //   const Layout = props.layout;
+          if (appWrapper && layout) {
+            const Layout = layout;
+            hydrateRoot(
+              document.getElementById("root"),
+              <Provider store={store}>
+                <AppWrapper>
+                  <ClientRoutes layout={Layout} />
+                </AppWrapper>
+              </Provider>
+            );
+          } else {
+            const Layout = layout;
+            hydrateRoot(
+              document.getElementById("root"),
+              <Provider store={store}>
+                <AppWrapper>
+                  <ClientRoutes layout={Layout} />
+                </AppWrapper>
+              </Provider>
+            );
+            // root.render(
+            //   <StrictMode>
+            //     <AppWrapper>
+            //       <Routes layout={Layout} />
+            //     </AppWrapper>
+            //   </StrictMode>
+            // );
+          }
+        }
       } else {
-        const Layout = layout;
-        hydrateRoot(
-          document.getElementById("root"),
-          <Provider store={store}>
-            <AppWrapper>
-              <ClientRoutes layout={Layout} />
-            </AppWrapper>
-          </Provider>
-        );
-        // root.render(
-        //   <StrictMode>
-        //     <AppWrapper>
-        //       <Routes layout={Layout} />
-        //     </AppWrapper>
-        //   </StrictMode>
-        // );
+        if (typeof window !== "undefined") {
+          if (meta?.app?.type !== "ssr") {
+            return <ClientRoutes />;
+          } else {
+            hydrateRoot(document.getElementById("root"), <ClientRoutes />);
+          }
+        }
       }
-    }
-  } else {
-    if (typeof window !== "undefined") {
-      hydrateRoot(document.getElementById("root"), <ClientRoutes />);
-    }
-  }
+    });
+  });
+};
+const AppWithRedux = (AppWrapper, Layout) => {
+  hydrateRoot(
+    document.getElementById("root"),
+    <Provider store={store}>
+      {AppWrapper ? (
+        <AppWrapper>
+          <AppWithReduxClientRoutes Layout={Layout} />
+        </AppWrapper>
+      ) : (
+        <AppWithReduxClientRoutes Layout={Layout} />
+      )}
+    </Provider>
+  );
+};
+
+const AppWithReduxClientRoutes = (Layout) => {
+  if (Layout) return <ClientRoutes layout={Layout} />;
+  return <ClientRoutes />;
 };
 
 const ServerApp = (
