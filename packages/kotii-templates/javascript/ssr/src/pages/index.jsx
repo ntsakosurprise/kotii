f4cb69e72a129a49988367e5b0ad464e782d00ad
ntsakosurprise <mashelesepru@gmail.com>
2024-06-12 03:09:44 +0200

    chore(kotii-templates): reference env variables

--- packages/kotii-templates/javascript/ssr/src/pages/index.jsx
+++ packages/kotii-templates/javascript/ssr/src/pages/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import { CONFIG } from "config";
 import { Head } from "kotii-scripts";
 import styled from "kotii-styled";
 import React from "react";
 import { AiFillFile, AiFillFolder } from "react-icons/ai/index.js";
 import { FaLongArrowAltRight } from "react-icons/fa/index.js";
 import { useDispatch, useSelector } from "react-redux";
+import CONFIG from "../config/environment_variables.js";
 import SVGConnections from "../shared/test.jsx";
 import * as actions from "../store/home/actions.js";
 
@@ -176,7 +176,8 @@ const PeopleList = (props) => {
   return null;
 };
 const Index = () => {
-  console.log("THE ENVIRONMENT CONFIG", CONFIG);
+  console.log("THE ENVIRONMENT CONFIG", CONFIG.GITHUB_APP_ID);
+  console.log("OUR NODE ENV", process.env.NODE_ENV);
   const peopleList = useSelector((state) => {
     console.log("STATE RECEIVED", state);
     return state.homeReducer.people;
