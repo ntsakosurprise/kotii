1cc5b3e952e2225069ff465955259b9524bf324b
ntsakosurprise <mashelesepru@gmail.com>
2024-07-04 03:04:58 +0200

    chore(kotii-templates): remove hot module on index file

--- packages/kotii-templates/javascript/ssr/src/index.js
+++ packages/kotii-templates/javascript/ssr/src/index.js
@@ -1,9 +1,5 @@
-if (import.meta.webpackHot) {
-  console.log("THE META.HOT");
-  import.meta.webpackHot.accept();
-}
 import App from "kotii-scripts";
-
 // import AppRoot from "";
 import { Layout, Root } from "Startup";
+
 App(Root, Layout);
