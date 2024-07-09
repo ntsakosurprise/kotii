739533074a40fce68ec8d58778a5efefb752a2a8
ntsakosurprise <mashelesepru@gmail.com>
2024-07-09 02:56:33 +0200

    chore(kotii-scripts): create server build for ssr

--- packages/kotii-scripts/plugins/buildsc/build.js
+++ packages/kotii-scripts/plugins/buildsc/build.js
@@ -5,6 +5,7 @@ class Build {
     this.init = methods.init;
     this.handleBuildScript = methods.handleBuildScript;
     this.doStaticSiteGeneration = methods.doStaticSiteGeneration;
+    this.doServerBuildGeneration = methods.doServerBuildGeneration;
     this.getWebPackConfig = methods.getWebPackConfig;
   }
 }
--- packages/kotii-scripts/plugins/buildsc/methods.js
+++ packages/kotii-scripts/plugins/buildsc/methods.js
@@ -8,14 +8,26 @@ methods.handleBuildScript = function (data) {
   console.log("THE DATA OF Build SCRIPTS", data);
   let setCall = data.callback;
   //   data.callback({ message: "Build plugin successfully called" });
+
   const self = this;
   // self.doStaticSiteGeneration({ callback: data.callback });
+
   self.emit({
     type: "context-app",
     data: {
       callback: (data) => {
-        console.log("BUILD CONTEXT APP RESPONSE", data?.routesObject[0]);
-        self.getWebPackConfig({ ...data, build: true }, setCall);
+        console.log("BUILD CONTEXT APP RESPONSE", data);
+        if (data?.contextApp) {
+          return self.doServerBuildGeneration({
+            callback: setCall,
+            targetSource: data.contextApp.appSrc,
+            targetMain: data.contextApp.appFolder,
+            destination: `${data.contextApp.appFolder}/build`,
+            targetNodeModules: `${data.contextApp.appNodeModules}`,
+          });
+        } else {
+          self.getWebPackConfig({ ...data, build: true }, setCall);
+        }
       },
       build: true,
       env: "production",
@@ -30,7 +42,6 @@ methods.getWebPackConfig = function (dataToConfig, setCall) {
     data: {
       payload: dataToConfig,
       callback: (data) => {
-        // console.log("THE DATA FROM WEBPACK CONFIG", data);
         self.doStaticSiteGeneration({
           callback: setCall,
           dataToConfig,
@@ -51,6 +62,22 @@ methods.doStaticSiteGeneration = function (data) {
       callback: (gotValue) => {
         console.log("STATIC GENERATION IS COMPLETED", gotValue);
         data.callback({ message: "Build plugin successfully called" });
+      },
+    },
+  });
+};
+
+methods.doServerBuildGeneration = function (data) {
+  console.log("DO SERVER SIDE RENDERING", data);
+  const self = this;
+
+  self.emit({
+    type: "generate-server-build",
+    data: {
+      payload: { build: "server-build", ...data },
+      callback: (gotValue) => {
+        console.log("STATIC GENERATION IS COMPLETED", gotValue);
+        data.callback({ message: "Server Build plugin successfully called" });
       },
     },
   });
