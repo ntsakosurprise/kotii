7f07eb7a6f3f3df7cc5b3830e72352f30bfcd01d
ntsakosurprise <mashelesepru@gmail.com>
2024-07-09 02:58:27 +0200

    chore(kotii-scripts): create ssr babel json

--- packages/kotii-scripts/plugins/index.js
+++ packages/kotii-scripts/plugins/index.js
@@ -17,6 +17,7 @@ import JsxToReact from "./jsxtoreact/index.js";
 import ReactView from "./react/reactview.js";
 import ReactToJsx from "./reacttojsx/index.js";
 import Scaffold from "./scaffold/index.js";
+import ServerBuild from "./server-build/index.js";
 import Ssg from "./ssg/index.js";
 import Start from "./start/index.js";
 import Watchr from "./watchr/index.js";
@@ -38,6 +39,7 @@ export { WebpackConfig };
 export { DevServer };
 export { ContextApp };
 export { FileRouter };
+export { ServerBuild };
 export { Env };
 export default {
   Interpreter,
@@ -62,4 +64,5 @@ export default {
   ReactView,
   Ssg,
   Env,
+  ServerBuild,
 };
