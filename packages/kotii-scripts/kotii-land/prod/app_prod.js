c95f863a39bb1c240dbe8cd64450e700e250a769
ntsakosurprise <mashelesepru@gmail.com>
2024-07-24 07:22:42 +0200

    chore(kotii-scripts): remove shebang for prod

--- packages/kotii-scripts/kotii-land/prod/app_prod.js
+++ packages/kotii-scripts/kotii-land/prod/app_prod.js
@@ -1,7 +1,6 @@
-// #!/usr/bin/env node
-
 import { anzii } from "anzii";
 import plugins from "../../plugins/index_pruned.js";
 process.env.DEBUG = "anzii:*";
 process.env.ANZII_SHOW_DEBUG_LOGS = "true";
+
 anzii(plugins);
