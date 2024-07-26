552668a49f46cd3cf498a7d4ba770a3af60f0319
ntsakosurprise <mashelesepru@gmail.com>
2024-07-26 21:58:24 +0200

    chore(kotii-scripts): add api for ssr template

---
+++ packages/kotii-templates/javascript/ssr/api/includes/routes.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    path: "/get-users",
+    method: "POST",
+    alias: "user",
+    type: "public",
+  },
+
+  //   {
+  //     path: "/test",
+  //     method: "POST",
+  //     type: "public",
+  //   },
+  //   {
+  //     path: "/login",
+  //     method: "POST",
+  //     type: "public",
+  //   },
+  //   {
+  //     path: "/verifygc",
+  //     alias: "gcaptcha",
+  //     method: "POST",
+  //     type: "public",
+  //   },
+];
---
+++ packages/kotii-templates/javascript/ssr/api/index.js
@@ -0,0 +1,2 @@
+import Users from "./plugins/users";
+export default { Users };
