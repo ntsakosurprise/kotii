9d53819d4acf9e501db1b4e9d3df94e1283138db
ntsakosurprise <mashelesepru@gmail.com>
2024-07-27 12:44:17 +0200

    chore(kotii-scripts): add option to add user plugins

--- packages/kotii-scripts/kotii-land/dev/app.js
+++ packages/kotii-scripts/kotii-land/dev/app.js
@@ -6,6 +6,7 @@ process.env.ANZII_SHOW_CLI_LOGS = "true";
 process.env.ANZII_OPEN_BROWSER = "true";
 process.env.DEBUG = "anzii:*";
 process.env.ANZII_SHOW_DEBUG_LOGS = "true";
+process.env.ANZII_SHOW_WILD_LOGS = "true";
 console.log("ANZII APP ESM", process.env);
 process.on("beforeExit", () => {
   console.log("THE PROCESS IS ABOUT TO EXIST");
@@ -13,4 +14,16 @@ process.on("beforeExit", () => {
 process.on("exit", () => {
   console.log("THE PROCESS HAS EXITED", anzii);
 });
-anzii(plugins);
+import("/kotii-user-api/plugins").then((imported) => {
+  console.log("THE IMPORTED USER PLUGINS", imported);
+  let userPlugins = imported.default;
+  console.log("USER PLUGINS", userPlugins);
+  if (userPlugins?.noApi && Object.keys(userPlugins).length === 1) {
+    console.log("API PLUGINS NO API");
+    anzii(plugins);
+  } else {
+    let pluginsCombined = { ...userPlugins, ...plugins };
+    console.log("API PLUGINS COMBINED", pluginsCombined);
+    anzii(pluginsCombined);
+  }
+});
