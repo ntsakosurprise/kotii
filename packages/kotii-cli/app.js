8b5d1e82e020035e4aa41ad6aae627f26699c69d
ntsakosurprise <mashelesepru@gmail.com>
2024-08-06 04:57:51 +0200

    chore(kotii-templates): references anzii from local path
    This is a temporary implementation to reference anzii locally, this was done
    as a solution to the issues encountered when referencing anzii as a tarball installation.
    
    Once we officially publish kotii packages to the regsitry, this implementation will be removed.

--- packages/kotii-cli/app.js
+++ packages/kotii-cli/app.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 process.argv.push("cli");
 const plugins = require("./plugins/index");
-const { anzii } = require("anzii");
-console.log("THE AN", anzii);
-anzii(plugins);
+// const { anzii } = require("anzii");
+const {
+  anzii,
+} = require("/Users/surprisemashele/Documents/Development/frameworks/anzii/packages/anzii/dist/index.cjs");
+
+const {
+  Template,
+} = require("/Users/surprisemashele/Documents/kotii/packages/kotii-templates/dist/index.cjs");
+console.log("THE AN", plugins);
+console.log(Template);
+anzii({ ...plugins, Template });
 //require("anzii")(plugins);
 // console.log(anzii)
