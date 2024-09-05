0fc30fee9a6a3551480fdb073e9e0ec87790a2d1
ntsakosurprise <mashelesepru@gmail.com>
2024-09-05 06:17:52 +0200

    chore(kotii-scripts): inspect cli arguments
    Checks arguments passed to the script to determine the course of action,
    this will run a production start scrtip if the parsed arguments contain
    start.
    
    In future, we will further add validations to it.

--- packages/kotii-scripts/kotii.cjs
+++ packages/kotii-scripts/kotii.cjs
@@ -48,7 +48,18 @@ const { register } = require("node:module");
 const { pathToFileURL } = require("node:url");
 process.env.NODE_ENV = "development";
 const parentURL = pathToFileURL(__filename);
+const cli = require("./cli.cjs");
+const { parseScriptArguments } = cli;
 
-console.log("THE REGISTER HOOK Dev");
-register("./compile/hooks_.js", parentURL);
-import("./kotii-land/dev/app.js");
+const commandToRun = parseScriptArguments()[0];
+
+if (commandToRun === "start") {
+  console.log("RUNNING SCRIPT:", commandToRun);
+  process.env.ANZII_KICK_OFF_MANUALLY = "true";
+  register("./compile/hooks_prod.js", parentURL);
+  import("./kotii-land/prod/app_prod.js");
+} else {
+  console.log("THE REGISTER HOOK Dev");
+  register("./compile/hooks_.js", parentURL);
+  import("./kotii-land/dev/app.js");
+}
