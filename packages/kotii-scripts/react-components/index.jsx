9e56355e2c10d22f5c92a8e2e7f9f0724a8d2a86
ntsakosurprise <mashelesepru@gmail.com>
2024-12-22 20:04:41 +0200

    chore(kotii-scripts): exports kotii hooks

--- packages/kotii-scripts/react-components/index.jsx
+++ packages/kotii-scripts/react-components/index.jsx
@@ -1,4 +1,4 @@
 import AppProvider, { useAppContext } from "./AppContext/index.jsx";
-import * as Hooks from "./Head/index.jsx";
 import Head, { HeadHelmet } from "./Head/index.jsx";
+import * as Hooks from "./hooks/index.jsx";
 export { Head, HeadHelmet, AppProvider, useAppContext, Hooks };
