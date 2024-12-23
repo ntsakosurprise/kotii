2fd396f560c9424ba218cd5bf78595c7405815cc
ntsakosurprise <mashelesepru@gmail.com>
2024-12-23 02:08:32 +0200

    chore(kotii-scripts): creates data access logic
    The data access logic in this commit will be used to acess data
    from the server and set it up for the current operation both on the
    client and server

--- packages/kotii-scripts/react-components/hooks/useUniversalEffect.js
+++ packages/kotii-scripts/react-components/hooks/useUniversalEffect.js
@@ -3,11 +3,23 @@ import { useState } from "react";
 const effect_id_prefix = "kotii_eff_id_";
 let currentEffectID = 1;
 
-export const useUniversalEffect = async (effect = null, dependencies = []) => {
+export const useUniversalEffect = async (
+  effect = null,
+  dependencies = [],
+  updaters = []
+) => {
   const effectID = `${effect_id_prefix}${currentEffectID}`;
-  const effectStore = useAppContext();
-  const [effectState, setEffectState] = useState(null);
-  const [effectErrorState, setErrorState] = useState(null);
+  const { effectsStore = null } = useAppContext();
+  console.log("THE EFFECTS STORE", effectsStore);
+  const { componentName } = effectsStore;
+  const effectResources = effectsStore[componentName];
+  console.log("THE EFFECTS RESOURCES", effectResources);
+  const { data, errors = null } = effectResources;
+  console.log("THE EFFECTS DATA ERRORS", data, errors);
+  const thisEffectErros = errors ? errors[effectID] || null : null;
+  const thisEffectData = data ? data[effectID] || null : null;
+  const [effectState, setEffectState] = useState(thisEffectData);
+  const [effectErrorState, setErrorState] = useState(thisEffectErros);
   if (!effect) {
     throw new Error(
       "useUniversalEfffect requires atleast an effect to be executed"
