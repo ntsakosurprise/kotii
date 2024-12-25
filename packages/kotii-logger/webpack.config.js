fcc3055bc06280840f13937e33a73d1f6092fd0d
ntsakosurprise <mashelesepru@gmail.com>
2024-12-25 11:55:46 +0200

    chore(kotii-logger): add dist folder to .gitignore

--- packages/kotii-logger/webpack.config.js
+++ packages/kotii-logger/webpack.config.js
@@ -1,78 +1,53 @@
-const path = require("path");
-const webpack = require("webpack");
-// const HTMLWebpackPlugin = require("html-webpack-plugin");
+import path from "path";
+import { fileURLToPath } from "url";
+import nodeExternals from "webpack-node-externals";
 
-module.exports = {
-  entry: "./src/index.tsx",
-  target: "web",
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+console.log("Webpack dir name", __dirname);
+console.log("webpack path", path.resolve(__dirname, "node_modules"));
+const isESM = process.env.NODE_MODE === "esm" ? true : false;
+
+const kotiiLogger = {
+  entry: "./index.js",
+  target: "node18",
   mode: "development",
-  devtool: "inline-source-map",
+
+  experiments: {
+    outputModule: false,
+  },
+  //devtool: "inline-source-map",
   output: {
-    path: path.join(__dirname),
-    filename: "index.js",
-    chunkFilename: "[id].js",
-    publicPath: "",
-    library: "kotii-ui",
-    libraryTarget: "umd",
+    path: path.resolve("dist"),
+    filename: isESM ? "index.mjs" : "index.cjs",
+    libraryTarget: isESM ? "module" : "commonjs2",
+    chunkFormat: isESM ? "module" : "commonjs",
   },
-  externals: {
-    react: {
-      root: "React",
-      commonjs2: "react",
-      commonjs: "react",
-      amd: "react",
-      umd: "react",
-    },
-    "react-dom": {
-      root: "ReactDOM",
-      commonjs2: "react-dom",
-      commonjs: "react-dom",
-      amd: "react-dom",
-      umd: "react-dom",
-    },
-  },
+  externals: [
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, "node_modules"),
+    }),
+    // nodeExternals({
+    //   modulesDir:
+    //     "/Users/surprisemashele/Documents/Development/frameworks/anzii/node_modules",
+    // }),
+  ],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-    alias: {
-      Config: "src/config/",
-      Components: "src/components/",
-      HOC: "src/hoc/",
-      Hooks: "src/hooks/index",
-      Context: "src/context/",
-      Utilities: "src/utils/index",
-      Constants: path.resolve(__dirname, "./src/constants/index"),
-      Assets: "src/assets/",
-      AppGlobals: "src/globals/index",
-      Types: "src/types",
-    },
+    extensions: [".js"],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader", "source-map-loader", "ts-loader"],
+        test: /\.(js)$/,
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+          //   "/Users/surprisemashele/Documents/Development/frameworks/anzii/node_modules",
+        ],
+        use: ["babel-loader"],
       },
-      // {
-      //   test: /\.html$/,
-      //   use: "html-loader",
-      // },
-      /*Choose only one of the following two: if you're using 
-      plain CSS, use the first one, and if you're using a
-      preprocessor, in this case SASS, use the second one*/
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      // {
-      //   test: /\.scss$/,
-      //   use: ["style-loader", "css-loader", "sass-loader"],
-      // },
     ],
+    exprContextCritical: false, // Temporary workaround
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env),
-    }),
-  ],
 };
+export default kotiiLogger;
