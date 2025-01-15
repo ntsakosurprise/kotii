e4f60ab356bfc7524c2b437d8009f12352a5a12e
ntsakosurprise <mashelesepru@gmail.com>
2025-01-15 04:15:33 +0200

    chore(kotii-scripts): temporary sets components paths
    This commit creates a temporary logic used to load the app components.
    The components paths are environment driven.

--- packages/kotii-scripts/plugins/react/methods.js
+++ packages/kotii-scripts/plugins/react/methods.js
@@ -112,13 +112,21 @@ methods.runReactView = function (data) {
       }
     }
 
+    let layoutStaticAbsolutePath =
+      process.env.NODE_ENV == "development"
+        ? `/src/components/startup/index.jsx`
+        : `/src/components/startup/index.js`;
     let layoutRoot = await self.doImport(
-      `/src/components/startup/index.jsx`,
+      `${layoutStaticAbsolutePath}`,
       true,
       false
     );
     if (!self.comps) {
-      self.comps = await self.doImport(`/kotii-land/dev/pages.js`, true, false);
+      let compsAbsolutePath =
+        process.env.NODE_ENV == "development"
+          ? `/kotii-land/dev/pages.js`
+          : `.kotii-land/pages.js`;
+      self.comps = await self.doImport(`${compsAbsolutePath}`, true, false);
     }
 
     let effectsStore = self.effectsData;
