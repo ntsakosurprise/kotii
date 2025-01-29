1b6d891d2b93a00f259e0f5a60c833713e884614
ntsakosurprise <mashelesepru@gmail.com>
2025-01-29 17:12:20 +0200

    chore(kotii-scripts): exports css resources

---
+++ packages/kotii-scripts/css/index.js
@@ -0,0 +1,5 @@
+import renderCssModules from "./css_to_js.js";
+import lessToCssConverter from "./less_to_css.js";
+import sassToCssConverter from "./sass_to_css.js";
+
+export { sassToCssConverter, lessToCssConverter, renderCssModules };
