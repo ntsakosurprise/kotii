71f00e7a9724025b2dda6790ee54d9133e9ce672
ntsakosurprise <mashelesepru@gmail.com>
2025-01-29 17:05:13 +0200

    chore(kotii-scripts): creates less to css converter
    The less to css converter will be used to convert less source code to
    css when nodejs encounters its format during compilation.

---
+++ packages/kotii-scripts/css/less_to_css.js
@@ -0,0 +1,16 @@
+import less from "less";
+import fs from "node:fs";
+
+export default (lessFile, lessFileName) => {
+  return new Promise((resolve) => {
+    less
+      .render(fs.readFileSync(lessFile, { encoding: "utf8" }), {
+        filename: lessFileName,
+        compress: false,
+      })
+      .then((output) => {
+        console.log("LESS OUTPU RESULT", output);
+        resolve(output.css);
+      });
+  });
+};
