762e5752e8a800d21caa8435a63f8243fee75d68
ntsakosurprise <mashelesepru@gmail.com>
2025-02-02 04:17:06 +0200

    chore(kotii-scripts): add css processing
    The previous implementation of babel's scoped-styles-plugin was made to
    process css files and save their content to a styles.json file.
    A new and an improved approach to processing css files was added to
    better process css files without saving their content as in the previous
    implementation.

--- packages/kotii-scripts/babel-plugins/scoped-styles-plugin/index.js
+++ packages/kotii-scripts/babel-plugins/scoped-styles-plugin/index.js
@@ -1,9 +1,48 @@
 import fs from "fs";
+import nativePath from "path";
+let assetsManifestData = null;
 // let cwd = process.cwd();
 const scopedStylesBabelPlugin = (babel, state) => {
+  if (!assetsManifestData) getStylesMap(state.cwd);
+  console.log("THE SCOOPE STATE", state, Object.keys(assetsManifestData));
+
   return {
     visitor: {
       ImportDeclaration(path) {
+        let importSpecifier = path.node.source.value;
+        if (
+          importSpecifier.indexOf(".scss") > 0 ||
+          importSpecifier.indexOf(".sass") > 0 ||
+          importSpecifier.indexOf(".css") > 0 ||
+          importSpecifier.indexOf(".less") > 0 ||
+          importSpecifier.indexOf(".styl") > 0
+        ) {
+          if (path.node.specifiers.length <= 0) {
+            return path.remove();
+          } else {
+            console.log("SCOOPED THE SPECIFIER", importSpecifier);
+            let fullPath = assetsManifestData[importSpecifier].fullPath;
+            let absoluteFilePath = fullPath.replace(
+              state.appSrc,
+              `${state.appBuildFolder}/src`
+            );
+
+            console.log("THE STATE", state.appSrc);
+            console.log("Scoped absolute path", absoluteFilePath);
+
+            let data = `export default {name: "test"}`;
+            console.log("New URL SCOOPED PLUGING", absoluteFilePath);
+            let extension = nativePath.extname(importSpecifier);
+            let cssModuleDataExport = `export default ${JSON.stringify(
+              assetsManifestData[importSpecifier].modules
+            )}`;
+            fs.writeFileSync(
+              absoluteFilePath.replace(extension, ".js"),
+              cssModuleDataExport
+            );
+            path.node.source.value = importSpecifier.replace(extension, ".js");
+          }
+        }
         // console.log("SCOPED-STYLES-PLUGIN", path.node.source.value);
         // console.log("SCOPED-STYLES-PLUGIN SPECIFIER", path.node.specifiers);
         // if (path.node.specifiers.length === 0) {
@@ -25,30 +64,19 @@ const scopedStylesBabelPlugin = (babel, state) => {
         //   return path.remove();
         // }
 
-        if (
-          path.node.source.value.indexOf(".scss") > 0 ||
-          path.node.source.value.indexOf(".sass") > 0 ||
-          path.node.source.value.indexOf(".css") > 0 ||
-          path.node.source.value.indexOf(".less") > 0 ||
-          (path.node.source.value.indexOf(".styl") > 0 &&
-            path.node.specifiers.length === 0)
-        ) {
-          // let sourceValue = path.node.source.value;
+        // let sourceValue = path.node.source.value;
 
-          // if (path.node.source.value.indexOf(".styl") > 0) {
-          //   manipulateStyles({
-          //     pathToStyles: resolve(`${state.appSrc}/src/styles`, sourceValue),
-          //     cwd: state.cwd,
-          //   });
-          // } else {
-          //   manipulateStyles({
-          //     pathToStyles: resolve(`${state.appSrc}/styles`, sourceValue),
-          //     cwd: state.cwd,
-          //   });
-          // }
-
-          return path.remove();
-        }
+        // if (path.node.source.value.indexOf(".styl") > 0) {
+        //   manipulateStyles({
+        //     pathToStyles: resolve(`${state.appSrc}/src/styles`, sourceValue),
+        //     cwd: state.cwd,
+        //   });
+        // } else {
+        //   manipulateStyles({
+        //     pathToStyles: resolve(`${state.appSrc}/styles`, sourceValue),
+        //     cwd: state.cwd,
+        //   });
+        // }
       },
     },
   };
@@ -95,6 +123,18 @@ const getStyles = (pathToStyles) => {
     encoding: "utf8",
   });
   return styles;
+};
+
+const getStylesMap = (kotiiAppPath) => {
+  let assetsPath = `${kotiiAppPath}/kotii-land/dev/styles-css-modules.json`;
+
+  if (fs.existsSync(assetsPath)) {
+    assetsManifestData = JSON.parse(
+      fs.readFileSync(assetsPath, {
+        encoding: "utf8",
+      })
+    );
+  }
 };
 
 export default scopedStylesBabelPlugin;
