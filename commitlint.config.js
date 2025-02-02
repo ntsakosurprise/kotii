9352a1621d0eeade0911a1b55c929209705fb902
ntsakosurprise <mashelesepru@gmail.com>
2025-02-02 04:06:11 +0200

    docs(root): remove commitlint subject rule
    Husky does not accept subject lines with uppercase as by our current
    configuration. This change removes the subject line limitation created
    in commitlint configuration file. The change was made to bypass the
    limitation.

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,11 +5,7 @@ export const rules = {
   "footer-leading-blank": [1, "always"],
   "footer-max-line-length": [2, "always", 100],
   "header-max-length": [2, "always", 100],
-  "subject-case": [
-    2,
-    "never",
-    ["sentence-case", "start-case", "pascal-case", "upper-case"],
-  ],
+  "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
   "subject-empty": [2, "never"],
   "subject-full-stop": [2, "never", "."],
   "type-case": [2, "always", "lower-case"],
