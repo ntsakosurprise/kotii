7a94e5bf71e5ed3cf608523fc380486e6d2e862a
ntsakosurprise <mashelesepru@gmail.com>
2025-02-22 18:58:56 +0200

    chore(kotii-scripts): formats plugin source code

--- packages/kotii-scripts/postcss-plugins/index.js
+++ packages/kotii-scripts/postcss-plugins/index.js
@@ -1,34 +1,32 @@
+import createRandomName from "./createRandomName.js";
+const plugin = (opts = {}) => {
+  console.log("POSTCSSPLUGIN OPTS", opts);
 
-import createRandomName from "./createRandomName.js"
-const plugin = (opts={})=>{
-  console.log("POSTCSSPLUGIN OPTS",opts)
-  
-  let cssFileStylesMap = {}
+  let cssFileStylesMap = {};
   return {
     postcssPlugin: "postcss-css-modules",
-    Rule:(ruleNode)=>{
-   
-      let selectorName = ruleNode.selector
-      if(selectorName[0] === "."){
-        let nameWithoutPeriod = selectorName.substr(1,selectorName.length)
-        let scopedClassName = `${selectorName}_${createRandomName(5)}_${createRandomName(3)}`
-        cssFileStylesMap[nameWithoutPeriod] = scopedClassName.substr(1,scopedClassName.length)
+    Rule: (ruleNode) => {
+      let selectorName = ruleNode.selector;
+      if (selectorName[0] === ".") {
+        let nameWithoutPeriod = selectorName.substr(1, selectorName.length);
+        let scopedClassName = `${selectorName}_${createRandomName(
+          5
+        )}_${createRandomName(3)}`;
+        cssFileStylesMap[nameWithoutPeriod] = scopedClassName.substr(
+          1,
+          scopedClassName.length
+        );
 
-
-        ruleNode.selector = scopedClassName
+        ruleNode.selector = scopedClassName;
       }
-      
     },
-    OnceExit(css){
-      console.log("THE JSON MAP",cssFileStylesMap)
-      css["modulesMap"] = cssFileStylesMap
-    }
-  }
-}
+    OnceExit(css) {
+      console.log("THE JSON MAP", cssFileStylesMap);
+      css["modulesMap"] = cssFileStylesMap;
+    },
+  };
+};
 
-  
-  
+plugin.postcss = true;
 
-plugin.postcss = true 
-
-export default plugin
+export default plugin;
