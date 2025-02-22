421e43eeb0c98399cc022d47ec6080585666b6ce
ntsakosurprise <mashelesepru@gmail.com>
2025-02-22 18:55:39 +0200

    chore(kotii-scripts): remove logic to copy files
    The logic to copy files as they were loaded by webpack is removed as
    of this commit, this is because webpack's this.emitFile was bugging-out.

--- packages/kotii-scripts/webpack-loaders/syncAssetsLoader.cjs
+++ packages/kotii-scripts/webpack-loaders/syncAssetsLoader.cjs
@@ -3,17 +3,16 @@ const fs = require("fs");
 let assetsManifestData = null;
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function (pngFile) {
+module.exports = function (content) {
   let options = this.getOptions();
+  const { referenceAssetsPath, assetsFile } = options;
+  // const { emitFile = false, emitPath } = extra;
 
   // const filePath = this.resource; // Webpack, get filepath
   const relativeFilePath = this._module.resourceResolveData.relativePath;
-  console.log("THE FILE RESULT", this._module.resourceResolveData);
+  // console.log("THE FILE RESULT", this._module.resourceResolveData);
 
-  let assetsPath = path.resolve(
-    options.referenceAssetsPath,
-    options.assetsFile
-  );
+  let assetsPath = path.resolve(referenceAssetsPath, assetsFile);
   if (!assetsManifestData) {
     if (fs.existsSync(assetsPath)) {
       assetsManifestData = JSON.parse(
@@ -24,21 +23,31 @@ module.exports = function (pngFile) {
     }
   }
 
-  console.log("THE FILE ID CONTENT", assetsManifestData);
+  // console.log("THE FILE ID CONTENT", assetsManifestData);
 
-  let content = getFileContent(relativeFilePath);
-  console.log("THE CONTENT", content);
+  let requestContent = getFileContent(relativeFilePath);
+  let exportString = `export default ${JSON.stringify(requestContent.content)}`;
+  // console.log("THE CONTENT", requestContent);
   // if (assetsManifestData[filePath]) {
   // fileIdContent = assetsManifestData[filePath].content;
   //}
-
-  return `export default ${JSON.stringify(content)}`;
+  // if (requestContent.inlined) return exportString;
+  // if (emitFile) {
+  //   console.log(
+  //     "EMIT FILE PATH",
+  //     `${requestContent.content}`,
+  //     "THE CONTENT",
+  //     content
+  //   );
+  //   this.emitFile(`${requestContent.content}`, content);
+  // }
+  return exportString;
 };
 
 const getFileContent = (relativeFilePath) => {
   let assetsKeys = Object.keys(assetsManifestData);
   let content = "";
-
+  // let inlined = false;
   for (
     let currentAssetIndex = 0;
     currentAssetIndex < assetsKeys.length;
@@ -47,10 +56,14 @@ const getFileContent = (relativeFilePath) => {
     let currentAssetMap = assetsManifestData[assetsKeys[currentAssetIndex]];
 
     if (currentAssetMap.pathContext.fileRelativePath === relativeFilePath) {
+      // inlined = currentAssetMap?.inlined || false;
       content = currentAssetMap.content;
       break;
     }
   }
 
-  return content;
+  return {
+    content,
+    // inlined,
+  };
 };
