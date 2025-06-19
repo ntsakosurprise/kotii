efd8a1ef676b08a7e3bf4f3989ce4c4224c4e980
ntsakosurprise <mashelesepru@gmail.com>
2025-06-19 03:11:51 +0200

    chore(kotii-scripts): check if effects key is set
    This change ensures that the app does not crash if __KOTII_EFFECTS_STATE
    is not set.

--- packages/kotii-scripts/react-components/AppContext/index.jsx
+++ packages/kotii-scripts/react-components/AppContext/index.jsx
@@ -5,10 +5,14 @@ const AppContext = React.createContext(null);
 const AppProvider = (props) => {
   const { layout, appWrapper } = props;
   const effectsStore =
-    typeof window !== "undefined"
+    typeof window !== "undefined" && window?.__KOTII_EFFECTS_STATE__
       ? JSON.parse(window.__KOTII_EFFECTS_STATE__)
-      : props.effectsStore;
-  typeof window !== "undefined" ? delete window.__KOTII_EFFECTS_STATE__ : null;
+      : props?.effectsStore
+      ? props.effectsStore
+      : null;
+  typeof window !== "undefined" && window?.__KOTII_EFFECTS_STATE__
+    ? delete window.__KOTII_EFFECTS_STATE__
+    : null;
   // effectsStore["isFirstTimeRun"] = true;
 
   return (
