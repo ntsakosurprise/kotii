77ee494cb717e1e01317c1bde1c1f5dc4df70322
ntsakosurprise <mashelesepru@gmail.com>
2025-06-26 19:43:05 +0200

    refactor(kotii-scripts): refactor loader exports

--- packages/kotii-scripts/webpack-loaders/index.js
+++ packages/kotii-scripts/webpack-loaders/index.js
@@ -1,5 +1,4 @@
 import eslintLoader from "./eslint-loader/index.js";
-import handleStylesLoader from "./handleStylesLoader.cjs";
-import syncAssetsLoader from "./syncAssetsLoader.cjs";
+import syncAssetsLoader from "./sync-assets-loader/index.cjs";
 import testStyles from "./testStyles.js";
-export { syncAssetsLoader, testStyles, eslintLoader, handleStylesLoader };
+export { syncAssetsLoader, testStyles, eslintLoader };
