0f5545816563b9e91e6e00ac118fd38452698706
ntsakosurprise <mashelesepru@gmail.com>
2025-07-11 05:31:31 +0200

    feat(kotii-scripts): remove console.log calls

--- packages/kotii-scripts/css/compare_css.js
+++ packages/kotii-scripts/css/compare_css.js
@@ -1,21 +1,16 @@
 //import path from "path";
 import postcss from "postcss";
+import { compareCssPlugin } from "../postcss-plugins/index.js";
 
-import {compareCssPlugin} from "../postcss-plugins/index.js";
+const compareCss = function (oldCssAst, newCssAst) {
+  const astBefore = newCssAst;
+  const astToTransform = astBefore;
 
-
-const compareCss = function (oldCssAst,newCssAst) {
- 
-  const astBefore = newCssAst
-  const astToTransform = astBefore
-  console.log("Copmaring TWO CSS trees::",newCssAst)
-  console.log("Copmaring TWO CSS OLD::",oldCssAst)
   return new Promise((resolve) => {
-    postcss([compareCssPlugin(oldCssAst,newCssAst)])
+    postcss([compareCssPlugin(oldCssAst, newCssAst)])
       .process(astToTransform)
       .then((result) => {
-        console.log("POST CSS PROCESSOR HAS COMPLETED", result);
-        resolve({ compared: true, update: result.root.update});
+        resolve({ compared: true, update: result.root.update });
       });
   });
 };
--- packages/kotii-scripts/css/stylus_to_css.js
+++ packages/kotii-scripts/css/stylus_to_css.js
@@ -1,22 +1,19 @@
-import stylus from "stylus";
 import fs from "node:fs";
+import stylus from "stylus";
 
 export default (stylusFile, stylusFileName) => {
-  console.log("THE STYLUS TO CSS", stylus, stylusFileName)
-  console.log("THEY STYLUS", stylus)
   return new Promise((resolve) => {
-    
-    stylus
-      .render(fs.readFileSync(stylusFile, { encoding: "utf8" }), {
+    stylus.render(
+      fs.readFileSync(stylusFile, { encoding: "utf8" }),
+      {
         filename: stylusFileName,
-        
-      },(err,output) => {
-        console.log("STYLUS OUTPUT RESULT", err,output);
-        if(err){
-          throw new Error(err)
+      },
+      (err, output) => {
+        if (err) {
+          throw new Error(err);
         }
         resolve(output);
-      })
-      
+      }
+    );
   });
 };
