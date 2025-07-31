7badfa917491f666d0e0fb04db77f79b194ea65d
ntsakosurprise <mashelesepru@gmail.com>
2025-07-31 06:03:42 +0200

    fix(kotii-scripts): add logic to prevent system crash

--- packages/kotii-scripts/kotii-land/prod/app_b.js
+++ packages/kotii-scripts/kotii-land/prod/app_b.js
@@ -31,7 +31,7 @@ const App = function () {
   userLayout = layout;
   const app = process.env.KOTII_APP_META;
   console.log("THE EFFECTSTORE RAW", window.__KOTII_EFFECTS_STATE__);
-  const effectsStore = JSON.parse(window.__KOTII_EFFECTS_STATE__);
+  const effectsStore = window?.__KOTII_EFFECTS_STATE__ ? JSON.parse(window.__KOTII_EFFECTS_STATE__) : null;
   loggas.appClient.debug("THE PROCESS.BROWSER.ENVS", process.env);
   let {
     type,
