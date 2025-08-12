65c10ca184c227c0546f4d3782dfc4d7f3db18bc
ntsakosurprise <mashelesepru@gmail.com>
2025-08-12 03:47:38 +0200

    feat(kotii-router): add Router context

--- packages/kotii-router/src/components/Router/Router.js
+++ packages/kotii-router/src/components/Router/Router.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { extractPathFromString } from "../../utils";
 
-const Router = () => {
-  return <div></div>;
+export const KotiiRouterContenxt = createContext();
+const Router = ({ children }) => {
+  const [cleanPath, setPath] = useState(
+    extractPathFromString(window.location.hash) || "/"
+  );
+
+  return (
+    <KotiiRouterContenxt.Provider value={{ path: cleanPath }}>
+      {children}
+    </KotiiRouterContenxt.Provider>
+  );
 };
+
 export default Router;
+
+export const navigate = (to) => {
+  window.location.hash = to;
+};
