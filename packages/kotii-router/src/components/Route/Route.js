fd02c59a9d53284e5925507de4d713db2841effa
ntsakosurprise <mashelesepru@gmail.com>
2025-08-12 05:29:21 +0200

    feat(kotii-router): use route matcher in Route component

--- packages/kotii-router/src/components/Route/Route.js
+++ packages/kotii-router/src/components/Route/Route.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
+import { matchRoutePattern } from "../../utils/index.js";
 import { KotiiRouterContenxt } from "../Router/Router.js";
 
 const Route = ({ component: Component, path }) => {
   const { path: navPath } = useContext(KotiiRouterContenxt);
-  if (navPath === path) {
+  const isRouteMatched = matchRoutePattern(path, navPath);
+  if (isRouteMatched) {
     return <Component />;
   }
   return null;
