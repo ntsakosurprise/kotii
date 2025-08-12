629d2a40469c19b4bbb442bb1e93a8f96e123eab
ntsakosurprise <mashelesepru@gmail.com>
2025-08-12 03:26:37 +0200

    feat(kotii-router): add onClick event to disable default behaviour

--- packages/kotii-router/src/components/Link/Link.js
+++ packages/kotii-router/src/components/Link/Link.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const Link = ({ goTo, isAbsolute = false, children, ...props }) => {
+  const handleOnclick = (e) => {
+    e.preventDefault();
+  };
   return (
-    <a href={!isAbsolute ? `#${goTo}` : goTo} {...props}>
+    <a
+      href={!isAbsolute ? `#${goTo}` : goTo}
+      onClick={handleOnclick}
+      {...props}
+    >
       {children}
     </a>
   );
