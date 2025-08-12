05d9f2aa52bf5c4458a37d8b7aa2c7d1134107cb
ntsakosurprise <mashelesepru@gmail.com>
2025-08-12 06:27:18 +0200

    feat(kotii-router): update Routes component

--- packages/kotii-router/src/components/Routes/Routes.js
+++ packages/kotii-router/src/components/Routes/Routes.js
@@ -1,6 +1,23 @@
 import React from "react";
-const Routes = () => {
-  return <></>;
+import { KotiiRouterContenxt } from "../Router/Router";
+const Routes = ({ children }) => {
+  const { path: currentPath, setParams } = useContext(KotiiRouterContenxt);
+
+  let elementToRender = null;
+
+  React.Children.forEach(children, (child) => {
+    if (elementToRender) return;
+
+    const { path } = child.props;
+    const match = matchPath(path, currentPath);
+
+    if (match) {
+      setParams(match.params);
+      elementToRender = child;
+    }
+  });
+
+  return elementToRender;
 };
 
 export default Routes;
