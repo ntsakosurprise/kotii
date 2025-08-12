f360cf72c7453db13fcd804848d658ef49b475ac
ntsakosurprise <mashelesepru@gmail.com>
2025-08-12 05:36:10 +0200

    feat(kotii-router): update match path logic

--- packages/kotii-router/src/utils/index.js
+++ packages/kotii-router/src/utils/index.js
@@ -11,6 +11,26 @@ export const navigate = (to) => {
 
 export const matchRoutePattern = (routeComponentPath, currentPath) => {
   if (routeComponentPath === currentPath) return true;
-  return false;
+
+  const routeSegments = routeComponentPath.split("/").filter(Boolean);
+  const currentSegments = currentPath.split("/").filter(Boolean);
+
+  if (routeSegments.length !== currentSegments.length) return null;
+
+  const params = {};
+
+  for (let i = 0; i < routeSegments.length; i++) {
+    const routeSegment = routeSegments[i];
+    const currentSegment = currentSegments[i];
+
+    if (routeSegment.startsWith(":")) {
+      const paramName = routeSegment.slice(1);
+      params[paramName] = currentSegment;
+    } else if (routeSegment !== currentSegment) {
+      return null;
+    }
+  }
+
+  return { params };
 };
 export { extractPathFromString, navigate, matchRoutePattern };
