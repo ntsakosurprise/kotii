21ce5aba5fafe517a87efd2125805f43bd84007e
ntsakosurprise <mashelesepru@gmail.com>
2025-08-14 21:24:39 +0200

    feat(kotii-router): add support for nested routes

--- packages/kotii-router/src/components/Routes/Routes.jsx
+++ packages/kotii-router/src/components/Routes/Routes.jsx
@@ -1,22 +1,73 @@
-import React, { useContext } from "react";
-import { matchRoutePattern } from "../../utils/index.js";
+import React, { useContext, useEffect } from "react";
+import {
+  cleanRouteUrl,
+  matchRoutePattern,
+  navigate,
+} from "../../utils/index.js";
 import { KotiiRouterContenxt } from "../Router/Router.jsx";
 const Routes = ({ children }) => {
-  const { path: currentPath, setParams } = useContext(KotiiRouterContenxt);
+  const {
+    path: currentPath,
+    setParams,
+    basePath,
+  } = useContext(KotiiRouterContenxt);
+
+  console.log("THE ROUTES COMPONENT");
 
   let elementToRender = null;
+  let theParams = null;
+  useEffect(() => {
+    console.log("About to set the params", theParams);
+    if (theParams) setParams(theParams);
+  }, [currentPath]);
 
-  React.Children.forEach(children, (child) => {
-    if (elementToRender) return;
-
+  for (let childIndex = 0; childIndex < children.length; childIndex++) {
+    if (elementToRender) break;
+    let child = children[childIndex];
     const { path } = child.props;
-    const match = matchRoutePattern(path, currentPath);
+    const fullUrl = cleanRouteUrl(`${basePath}/${path}`);
+    const match = matchRoutePattern(fullUrl, currentPath);
 
     if (match) {
-      setParams(match.params);
+      console.log("REACT CHILD ELEMENT", match);
+      theParams = match?.params ? match.params : null;
       elementToRender = child;
+      elementToRender = (
+        <KotiiRouterContenxt.Provider
+          value={{
+            path: currentPath,
+            navigate,
+            setParams,
+            params: theParams,
+            basePath: fullUrl,
+          }}
+        >
+          {child}
+        </KotiiRouterContenxt.Provider>
+      );
+
+      break;
+
+      // theParams = match.params
     }
-  });
+  }
+
+  // React.Children.forEach(children, (child) => {
+  //   console.log("REACT CHILD ELEMENT",child.props.path)
+  //   if (elementToRender) return;
+
+  //   const { path } = child.props;
+  //   const match = matchRoutePattern(path, currentPath);
+
+  //   if (match) {
+  //     console.log("REACT CHILD ELEMENT",match)
+  //     elementToRender = child;
+  //    theParams = match?.params ? match.params: null;
+
+  //     // theParams = match.params
+
+  //   }
+  // });
 
   return elementToRender;
 };
