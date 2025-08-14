4a5af318f36638b231a8b49a362091340240fa74
ntsakosurprise <mashelesepru@gmail.com>
2025-08-14 21:01:50 +0200

    feat(kotii-scripts): add kotii-router routing

--- packages/kotii-scripts/kotii-land/dev/build.js
+++ packages/kotii-scripts/kotii-land/dev/build.js
@@ -1,75 +1,91 @@
+import { Route, Router, Routes } from "kotii-router";
 import React from "react";
-import { Route, Router, Switch as Routes } from "wouter";
 import { useAppContext } from "../../react-components/index.jsx";
-const Wrapper = props => {
+const Wrapper = (props) => {
   //const Component = props.component;
-  return <div style={{
-    paddingLeft: "2%",
-    paddingTop: "2vh",
-    fontFamily: '"Roboto", sans-serif'
-  }}>
+  return (
+    <div
+      style={{
+        paddingLeft: "2%",
+        paddingTop: "2vh",
+        fontFamily: '"Roboto", sans-serif',
+      }}
+    >
       {props.children}
-    </div>;
+    </div>
+  );
 };
-const ClientRoutes = props => {
+const ClientRoutes = (props) => {
   let astRoutes = typeof routes === "undefined" ? [] : routes;
   let astComps = typeof comps === "undefined" ? {} : comps;
-  const {
-    layout
-  } = useAppContext();
+  const { layout } = useAppContext();
   // const AppWrapper = props.wrapper;
   // console.log("THE CLIENT ROUTES", layout);
-  const Layout = layout ? layout : () => {
-    return <></>;
-  };
-  return <Router>
+  const Layout = layout
+    ? layout
+    : () => {
+        return <></>;
+      };
+  return (
+    <Router>
       <Layout>
         <Routes>
           {astRoutes.map((r, index) => {
-          let Component = astComps[r.component];
-          const ComponentWrapped = () => {
-            return <Wrapper>
+            let Component = astComps[r.component];
+            const ComponentWrapped = () => {
+              return (
+                <Wrapper>
                   <Component />
-                </Wrapper>;
-          };
-          return <Route
-          // {...rest}
-          key={index} path={r.path} component={ComponentWrapped}
-          // render={(props) => {
-          //   return <Component {...props} />;
-          // }}
-          />;
-        })}
+                </Wrapper>
+              );
+            };
+            return (
+              <Route
+                // {...rest}
+                key={index}
+                path={r.path}
+                component={ComponentWrapped}
+                // render={(props) => {
+                //   return <Component {...props} />;
+                // }}
+              />
+            );
+          })}
         </Routes>
       </Layout>
-    </Router>;
+    </Router>
+  );
 };
-const RoutesAsServerRoutes = props => {
-  const {
-    goodies = {}
-  } = props;
+const RoutesAsServerRoutes = (props) => {
+  const { goodies = {} } = props;
   // const {routes=[], comps={}} = goodies
   const gRoutes = goodies?.routes || [];
   const gComps = goodies?.comps || {};
-  const {
-    layout
-  } = useAppContext();
-  const Layout = layout ? layout : () => {
-    return <></>;
-  };
-  return <Layout>
+  const { layout } = useAppContext();
+  const Layout = layout
+    ? layout
+    : () => {
+        return <></>;
+      };
+  return (
+    <Layout>
       <Routes>
         {gRoutes.map((r, index) => {
-        let Component = gComps[r.component];
-        let ComponentWrapped = () => {
-          return <Wrapper>
+          let Component = gComps[r.component];
+          let ComponentWrapped = () => {
+            return (
+              <Wrapper>
                 <Component />
-              </Wrapper>;
-        };
-        return <Route key={index} path={r.path} component={ComponentWrapped} />;
-      })}
+              </Wrapper>
+            );
+          };
+          return (
+            <Route key={index} path={r.path} component={ComponentWrapped} />
+          );
+        })}
       </Routes>
-    </Layout>;
+    </Layout>
+  );
 };
 
 // export { RoutesAsServerRoutes, routes };
