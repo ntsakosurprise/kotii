62ec1572381f83c55b55289b58f3677ca237920b
ntsakosurprise <mashelesepru@gmail.com>
2025-08-14 21:17:05 +0200

    feat(kotii-router): add cleanUrl util

--- packages/kotii-router/src/utils/index.js
+++ packages/kotii-router/src/utils/index.js
@@ -5,28 +5,34 @@ export const extractPathFromString = (pathString) => {
   return pathString;
 };
 
+export const cleanRouteUrl = (pathString) => {
+  return pathString.replace(/\/+/g, "/").replace(/\/$/, "") || "/";
+};
+
 export const navigate = (to) => {
   //   window.location.hash = to;
+  console.log("GOINT TO");
   window.history.pushState({}, "", to);
   const navEvent = new PopStateEvent("popstate");
   window.dispatchEvent(navEvent);
 };
 
 export const matchRoutePattern = (routeComponentPath, currentPath) => {
-  if (routeComponentPath === currentPath) return true;
+  // if (routeComponentPath === currentPath) return true;
 
   const routeSegments = routeComponentPath.split("/").filter(Boolean);
   const currentSegments = currentPath.split("/").filter(Boolean);
 
   if (routeSegments.length !== currentSegments.length) return null;
 
-  const params = {};
+  let params = null;
 
   for (let i = 0; i < routeSegments.length; i++) {
     const routeSegment = routeSegments[i];
     const currentSegment = currentSegments[i];
 
     if (routeSegment.startsWith(":")) {
+      if (!params) params = {};
       const paramName = routeSegment.slice(1);
       params[paramName] = currentSegment;
     } else if (routeSegment !== currentSegment) {
