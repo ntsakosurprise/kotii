2ed3409c8030f9ac7375ac65ed9a4d9ac45027a8
ntsakosurprise <mashelesepru@gmail.com>
2025-08-20 22:50:00 +0200

    feat(kotii-lazy): update LazySuspense component

--- packages/kotii-lazy/src/components/Suspense/Suspense.jsx
+++ packages/kotii-lazy/src/components/Suspense/Suspense.jsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { navigate } from "../../utils/index.js";
 
-const Suspense = ({ to, isAbsolute = false, children, ...props }) => {
-  const handleOnclick = (e) => {
-    e.preventDefault();
-    console.log("LINK NAVIGATION TO", to);
-    navigate(to);
-  };
-  return (
-    <a href={to} onClick={handleOnclick} {...props}>
-      {children}
-    </a>
-  );
-};
+// const Suspense = ({ fallback }) => {
+
+//   return (
+//     <a href={to} onClick={handleOnclick} {...props}>
+//       {children}
+//     </a>
+//   );
+// };
+
+class Suspense extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: null, lazyError: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    if (error instanceof Promise) {
+      return { lazyError: error };
+    }
+    return { hasError: error };
+  }
+
+  componentDidCatch(error) {
+    if (error instanceof Promise) {
+      this.state.lazyError.then(() => {
+        this.setState({ lazyError: null });
+      });
+    }
+  }
+
+  render() {
+    if (this.state.lazyError) {
+      return this.props.fallback;
+    }
+    if (this.state.hasError) {
+      return <div>An error occured rendering component</div>;
+    }
+
+    return this.props.children;
+  }
+}
 
 export default Suspense;
