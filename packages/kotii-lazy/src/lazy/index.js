15fd2e377556357dbea8f4f044ac2b5986781c90
ntsakosurprise <mashelesepru@gmail.com>
2025-08-24 02:41:50 +0200

    feat(kotii-lazy): update lazy logic for ssr preload

--- packages/kotii-lazy/src/lazy/index.js
+++ packages/kotii-lazy/src/lazy/index.js
@@ -1,19 +1,19 @@
 export default (componentLoader) => {
+  console.log("THE COMPONENT LOADER", componentLoader);
   let LoadedComponent = null;
   let loadModulePromise = null;
-  let state = "loading";
 
   async function loadModule() {
     if (!LoadedComponent) {
-      loadModulePromise = componentLoader().then((componentModule) => {
-        return componentModule.default || componentModule;
-      });
-      await loadModulePromise;
+      loadModulePromise = await componentLoader();
+      LoadedComponent = loadModulePromise?.default || loadModulePromise;
+      console.log("THE LOADED", loadModulePromise);
     }
     return LoadedComponent;
   }
 
   function Lazy() {
+    console.log("Lazy runs");
     if (!LoadedComponent) {
       loadModulePromise = componentLoader().then((componentModule) => {
         return componentModule.default || componentModule;
