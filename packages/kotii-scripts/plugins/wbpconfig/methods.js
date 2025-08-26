c8009efdabe4df6fcd4f9c1cd34f76e0f3423df6
ntsakosurprise <mashelesepru@gmail.com>
2025-08-26 07:01:41 +0200

    feat(kotii-scripts): set client env for lazy load

--- packages/kotii-scripts/plugins/wbpconfig/methods.js
+++ packages/kotii-scripts/plugins/wbpconfig/methods.js
@@ -7,6 +7,7 @@ import fs from "fs";
 import path from "path";
 import postcss from "postcss";
 import postcssNested from "postcss-nested";
+import { env } from "process";
 import tailwindcss from "tailwindcss";
 import WebSocket, { WebSocketServer } from "ws";
 import {
@@ -167,6 +168,9 @@ methods.configureWebPack = function (
   );
   envs.stringified["KOTII_SHOW_DEBUG_LOGS"] = true;
   envs.stringified["KOTII_APP_URL"] = JSON.stringify(certDomainConfig.APP_URL);
+  env.stringified["KOTII_USE_LAZY"] = JSON.stringify(
+    process?.useLazyLoad ? true : false
+  );
 
   self.debug("THE APP ENVS", envs);
   // self.addImportLineTCSSModulesJs()
