6ea398e35b00b474164d2676cea081f06f44ad60
ntsakosurprise <mashelesepru@gmail.com>
2025-08-28 03:57:09 +0200

    fix(kotii-lazy): refactor lazy function

--- packages/kotii-lazy/src/lazy/index.js
+++ packages/kotii-lazy/src/lazy/index.js
@@ -16,11 +16,24 @@ export default (componentLoader) => {
   function Lazy(props) {
     console.log("Lazy runs", props);
     if (!LoadedComponent) {
-      loadModulePromise = componentLoader().then((componentModule) => {
+      console.log("Component is not loaded");
+      loadModulePromise = componentLoader();
+
+      loadModulePromise.then((componentModule) => {
+        console.log("LAZY COMPONENT IS RESOLVED", componentModule?.default);
+        LoadedComponent = componentModule.default || componentModule;
         return componentModule.default || componentModule;
       });
+      console.log(
+        "WE ARE ABOUT TO THROW",
+        "typeof",
+        typeof loadModulePromise,
+        "instanceof",
+        loadModulePromise instanceof Promise
+      );
       throw loadModulePromise;
     }
+    console.log("THE COMPONENT IS LOADED");
     return <LoadedComponent {...props} />;
   }
   Lazy.preload = loadModule;
