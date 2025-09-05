f21bfc4b9d10615503be8cdcfc57f87e9240069f
ntsakosurprise <mashelesepru@gmail.com>
2025-09-05 02:22:05 +0200

    feat(kotii-scripts): create event handler for markdown pages

--- packages/kotii-scripts/plugins/markdownr/markdownr.js
+++ packages/kotii-scripts/plugins/markdownr/markdownr.js
@@ -1,9 +1,10 @@
+import methods from "./methods";
 class Markdownr {
   constructor(pao) {
     this.pao = pao;
+
     this.init = methods.init;
-    this.handleUpdate = methods.handleUpdate;
-    this.getDataLocal = methods.getDataLocal;
+    this.handleMardownPages = methods.handleMardownPages;
   }
 }
 export default Markdownr;
