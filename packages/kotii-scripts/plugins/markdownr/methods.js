80f7ccc61ecccd252c56043bdfb6c3f11703e263
ntsakosurprise <mashelesepru@gmail.com>
2025-09-10 03:54:34 +0200

    feat(kotii-scripts): set and run doDuring function

--- packages/kotii-scripts/plugins/markdownr/methods.js
+++ packages/kotii-scripts/plugins/markdownr/methods.js
@@ -8,21 +8,31 @@ methods.init = function () {
 };
 methods.handleMarkdown = function (data) {
   const self = this;
-  const { callback, payload } = data;
+  const { callback, doDuring = () => {}, payload } = data;
   const { markdownPages } = payload;
+  console.log("THE DATA OBJECT", data);
 
-  let createdPages = self.getPagesMarkdownContent(markdownPages);
+  let createdPages = self.getPagesMarkdownContent(markdownPages, doDuring);
 
   callback(createdPages);
 };
 
-methods.getPagesMarkdownContent = function (pages) {
+methods.getPagesMarkdownContent = function (pages, doDuring) {
   const self = this;
   const { MarkdownLoader } = self;
 
   const filteredPages = pages.map((pagePath) => {
-    return MarkdownLoader({ resource: pagePath });
+    let markResults = MarkdownLoader({
+      resource: pagePath,
+      isServerMode: true,
+    });
+    let route = doDuring(pagePath);
+
+    console.log("THE MARKDOWN RETURN", route);
+
+    return { ...markResults, ...route };
   });
+
   self.debug("THE FILTERED PAGES", filteredPages);
   return filteredPages;
 };
