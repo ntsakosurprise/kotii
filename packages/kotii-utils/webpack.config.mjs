f4bc128a4813010bf6e659dce846fe332c70919c
ntsakosurprise <mashelesepru@gmail.com>
2025-09-14 08:53:34 +0200

    feat(kotii-utils): update webpack config with esm

---
+++ packages/kotii-utils/webpack.config.mjs
@@ -0,0 +1,69 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+console.log("Webpack dir name", __dirname);
+console.log("webpack path", path.resolve(__dirname, "node_modules"));
+const isESM = process.env.NODE_MODE === "esm" ? true : false;
+
+const kotiiRouter = {
+  entry: "./src/index.js",
+  target: "web",
+  mode: "development",
+  devtool: "inline-source-map",
+
+  experiments: {
+    outputModule: isESM ? true : false,
+  },
+  //devtool: "inline-source-map",
+  output: {
+    path: path.resolve("dist"),
+    filename: isESM ? "index.mjs" : "index.cjs",
+    libraryTarget: isESM ? "module" : "commonjs2",
+    chunkFormat: isESM ? "module" : "commonjs",
+
+    module: isESM ? true : false,
+  },
+  externals: [
+    {
+      react: "react",
+      "react-dom": "react-dom",
+    },
+
+    // nodeExternals({
+    //   modulesDir:
+    //     "/Users/surprisemashele/Documents/Development/frameworks/anzii/node_modules",
+    // }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      // {
+      //   test: /\.html$/,
+      //   use: "html-loader",
+      // },
+      /*Choose only one of the following two: if you're using 
+      plain CSS, use the first one, and if you're using a
+      preprocessor, in this case SASS, use the second one*/
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "css-loader", "sass-loader"],
+      // },
+    ],
+  },
+};
+
+export default kotiiRouter;
