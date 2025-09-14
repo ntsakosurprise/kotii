9e1974d60e5b6bee8f211f19a8af724f9ed8a1ad
ntsakosurprise <mashelesepru@gmail.com>
2025-09-14 09:06:32 +0200

    feat(kotii-react-modules): update exports

--- packages/kotii-react-modules/src/components/index.js
+++ packages/kotii-react-modules/src/components/index.js
@@ -1,6 +1,6 @@
 import MarkdownElement from "./MarkdownElement/index.js";
-import MarkdownHeader from "./MarkdownHeader/index.js";
+
 import MarkdownRender from "./MarkdownRender/index.js";
 import MarkdownSidebar from "./MarkdownSidebar/index.js";
 
-export { MarkdownElement, MarkdownHeader, MarkdownRender, MarkdownSidebar };
+export { MarkdownElement, MarkdownRender, MarkdownSidebar };
