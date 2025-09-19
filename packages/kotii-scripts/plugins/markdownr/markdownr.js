ad9a0cfd6fd6c4098d22169d44cb658ad2a2a6ac
ntsakosurprise <mashelesepru@gmail.com>
2025-09-19 18:14:05 +0200

    feat(kotii-scripts): import serverLoader from kotii-markdown

--- packages/kotii-scripts/plugins/markdownr/markdownr.js
+++ packages/kotii-scripts/plugins/markdownr/markdownr.js
@@ -1,10 +1,10 @@
-import { MarkdownLoader } from "kotii-markdown";
+import { MarkdownLoader, serverLoader } from "kotii-markdown";
 import methods from "./methods.js";
 
 class Markdownr {
   constructor(pao) {
     this.pao = pao;
-    this.MarkdownLoader = MarkdownLoader;
+    this.markdownLoader = serverLoader;
 
     this.init = methods.init;
     this.handleMarkdown = methods.handleMarkdown;
