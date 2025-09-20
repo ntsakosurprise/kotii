01153661ff9b0b62f39d36161ab2b8602bf56f33
ntsakosurprise <mashelesepru@gmail.com>
2025-09-20 04:19:56 +0200

    feat(kotii-scripts): update markdown components

--- packages/kotii-scripts/react-components/MarkdownVideo/index.jsx
+++ packages/kotii-scripts/react-components/MarkdownVideo/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 
 const MarkdownVideo = ({ children, isNotChildVideo = false }) => {
+  console.log("THE CHILDREN", children, isNotChildVideo);
   if (isNotChildVideo) return <video>{children}</video>;
   return <div>{children}</div>;
 };
--- packages/kotii-scripts/react-components/StandardComponent/index.jsx
+++ packages/kotii-scripts/react-components/StandardComponent/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import styled from "kotii-styled";
 import PropTtypes from "prop-types";
+import React from "react";
 
 const StyledStandardComponent = styled("div")(() => ({
   width: "100%",
