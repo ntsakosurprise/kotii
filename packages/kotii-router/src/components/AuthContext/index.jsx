554ce301e8b577914342052d5442db9bf1a81f5d
ntsakosurprise <mashelesepru@gmail.com>
2025-09-28 07:57:26 +0200

    feat(kotii-router): add logout actions

--- packages/kotii-router/src/components/AuthContext/index.jsx
+++ packages/kotii-router/src/components/AuthContext/index.jsx
@@ -1,12 +1,29 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 const AuthContext = React.createContext(null);
 
-const AuthProvider = ({ defaultUser = null, children }) => {
+const AuthProvider = ({
+  defaultUser = null,
+  children,
+  onLogin = () => {},
+  onLoginOut = () => {},
+}) => {
   const [user, setUser] = useState(defaultUser);
   const authLogin = (authUser) => setUser(authUser);
   const authLogout = () => setUser(null);
+  const runOnLogin = () => {
+    onLogin();
+  };
+  const runOnLogout = () => {
+    onLoginOut();
+  };
+
+  useEffect(() => {
+    if (!user) runOnLogout();
+    runOnLogin();
+  }, [user]);
   return (
     <AuthContext.Provider
       value={{ login: authLogin, user, logout: authLogout }}
