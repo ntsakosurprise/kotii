35f42cc4ede8d4514cce1027e0b48aec32f58dd9
ntsakosurprise <mashelesepru@gmail.com>
2025-09-30 06:49:43 +0200

    feat(kotii-auth): expose useAuthRegisterActions

--- packages/kotii-auth/src/hooks/useParams.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { KotiiRouterContenxt } from "../components/Router/Router.jsx";
-
-export default () => {
-  const { params } = useContext(KotiiRouterContenxt);
-  return params;
-};
--- packages/kotii-auth/src/hooks/useRedirect.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { KotiiRouterContenxt } from "../components/Router/Router.jsx";
-
-export default () => {
-  const { navigateByReplace } = useContext(KotiiRouterContenxt);
-  return navigateByReplace;
-};
