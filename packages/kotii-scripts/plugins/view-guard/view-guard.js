13be2629af9ed2687bdfc99179c6e45d4f888937
ntsakosurprise <mashelesepru@gmail.com>
2025-10-04 01:53:54 +0200

    feat(kotii-scripts): update server ViewGuard plugin

--- packages/kotii-scripts/plugins/view-guard/view-guard.js
+++ packages/kotii-scripts/plugins/view-guard/view-guard.js
@@ -1,21 +1,14 @@
 import * as methods from "./methods.js";
 
-import jwt from "jsonwebtoken";
-
 class ViewGuard {
   constructor(pao) {
     this.pao = pao;
-    this.jwt = jwt;
-    this.key = "randomefyerczhk5r325xsr6";
 
     // // methods
 
     this.init = methods.init;
-    this.jwtSign = methods.jwtSign;
-    this.jwtVerify = methods.jwtVerify;
+
     this.handleViewAuthentication = methods.handleViewAuthentication;
-    this.handleCreateToken = methods.handleCreateToken;
-    this.handleVerifyToken = methods.handleVerifyToken;
   }
 }
 
