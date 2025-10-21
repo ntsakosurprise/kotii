2d6a01e4d09deb70bf65c4b5b117e597e764f053
ntsakosurprise <mashelesepru@gmail.com>
2025-10-21 04:46:27 +0200

    refactor(kotii-scripts): close watchers on shutdown

--- packages/kotii-scripts/plugins/wbpconfig/methods.js
+++ packages/kotii-scripts/plugins/wbpconfig/methods.js
@@ -1754,11 +1754,12 @@ methods.closeWatchersOnShutdown = function () {
   const self = this;
 
   self.debug("ANZII JS: Closing Watchers", process.env.IS_WATCHING_FILE);
+  // self.closeWatcher();
+  // if (process.env.IS_WATCHING_FILE) {
+  //   process.env["IS_WATCHING_FILE"] = false;
+  //   self.closeWatcher();
+  // }
   self.closeWatcher();
-  if (process.env.IS_WATCHING_FILE) {
-    process.env["IS_WATCHING_FILE"] = false;
-    self.closeWatcher();
-  }
 };
 
 methods.runWebpackCompiler = function () {
