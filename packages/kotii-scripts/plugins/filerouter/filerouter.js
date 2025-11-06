a5dbe7aa3bea4fe5116f14897e82c13d24b118a9
ntsakosurprise <mashelesepru@gmail.com>
2025-11-06 22:44:48 +0200

    chore(kotii-scripts): add lazy import methods

--- packages/kotii-scripts/plugins/filerouter/filerouter.js
+++ packages/kotii-scripts/plugins/filerouter/filerouter.js
@@ -93,8 +93,12 @@ class FileRouter {
     this.astAddNodeMarkdown = methods.astAddNodeMarkdown;
     this.createMarkdownRoutesAst = methods.createMarkdownRoutesAst;
     this.astMarkdownUtils = methods.astMarkdownUtils;
-    this.addMarkdownRenderComponent = methods.addMarkdownRenderComponent;
-    this.addLazyImporter = methods.addLazyImporter;
+    this.replaceKotiiJsFilesContent = methods.replaceKotiiJsFilesContent;
+    this.getCentralFilesContent = methods.getCentralFilesContent;
+    this.isImportSetInAst = methods.isImportSetInAst;
+    this.removeDyanimcImportDeclaration =
+      methods.removeDyanimcImportDeclaration;
+    this.isLazyLoadedImportVariable = methods.isLazyLoadedImportVariable;
   }
 }
 export default FileRouter;
