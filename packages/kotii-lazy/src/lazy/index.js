897ed864b5cbcab172e880aa2a17e91a75202618
ntsakosurprise <mashelesepru@gmail.com>
2025-12-02 18:16:09 +0200

    docs(packages): add missing package.json

--- packages/kotii-lazy/src/lazy/index.js
+++ packages/kotii-lazy/src/lazy/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 export default (componentLoader) => {
-  console.log("THE COMPONENT LOADER", componentLoader);
   let LoadedComponent = null;
   let loadModulePromise = null;
 
@@ -8,32 +7,22 @@ export default (componentLoader) => {
     if (!LoadedComponent) {
       loadModulePromise = await componentLoader();
       LoadedComponent = loadModulePromise?.default || loadModulePromise;
-      console.log("THE LOADED", loadModulePromise);
     }
     return LoadedComponent;
   }
 
   function Lazy(props) {
-    console.log("Lazy runs", props);
     if (!LoadedComponent) {
-      console.log("Component is not loaded");
       loadModulePromise = componentLoader();
 
       loadModulePromise.then((componentModule) => {
-        console.log("LAZY COMPONENT IS RESOLVED", componentModule?.default);
         LoadedComponent = componentModule.default || componentModule;
         return componentModule.default || componentModule;
       });
-      console.log(
-        "WE ARE ABOUT TO THROW",
-        "typeof",
-        typeof loadModulePromise,
-        "instanceof",
-        loadModulePromise instanceof Promise
-      );
+
       throw loadModulePromise;
     }
-    console.log("THE COMPONENT IS LOADED");
+
     return <LoadedComponent {...props} />;
   }
   Lazy.preload = loadModule;
