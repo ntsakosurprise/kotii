ebcaa56d6fb9465960a77325ffbb1038e6bac942
ntsakosurprise <mashelesepru@gmail.com>
2025-12-04 05:10:57 +0200

    feat(kotii): add scripts and run scripts from kotii-create-time

--- packages/kotii/kotii_create_time.cjs
+++ packages/kotii/kotii_create_time.cjs
@@ -1,19 +1,39 @@
-const beginCreations = (dataType, path) => {
-  console.log("GET FOREING", dataType, path);
-  return new Promise((resolve) => {
-    switch (dataType.toLowerCase()) {
-      case "json":
-        return resolve(getJSON(path));
-      case "xml":
-        return getXML(path).then((data) => {
-          resolve(data);
-        });
-      case "csv":
-        return resolve(getCSV(path));
-      default:
-        "";
-    }
-  });
+const beginCreations = (commandToRun) => {
+  const { dev, build, ssg } = require("kotii-create-time");
+
+  register("./compile/hooks_.js", parentURL);
+  getAndSetEnvironmentVariables(
+    process.env.NODE_ENV
+      ? process.env.NODE_ENV != "development"
+        ? "development"
+        : process.env.NODE_ENV
+      : "development"
+  );
+  switch (commandToRun) {
+    case "dev":
+      return dev();
+    case "build":
+      return build();
+    case "static":
+      return ssg();
+    default:
+      throw new Error("KotiiJS was started with an unrecognised command");
+  }
+
+  //   return new Promise((resolve) => {
+  //     switch (dataType.toLowerCase()) {
+  //       case "json":
+  //         return resolve(getJSON(path));
+  //       case "xml":
+  //         return getXML(path).then((data) => {
+  //           resolve(data);
+  //         });
+  //       case "csv":
+  //         return resolve(getCSV(path));
+  //       default:
+  //         "";
+  //     }
+  //   });
 };
 
 module.exports = { beginCreations };
