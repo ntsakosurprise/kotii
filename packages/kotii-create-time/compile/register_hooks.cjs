29b9a6425b801fdf1f449139f5e0a4640929e452
ntsakosurprise <mashelesepru@gmail.com>
2025-12-04 05:17:50 +0200

    feat(kotii-create-time): update register hooks to reference hooks file

--- packages/kotii-create-time/compile/register_hooks.cjs
+++ packages/kotii-create-time/compile/register_hooks.cjs
@@ -1,9 +1,4 @@
 const { register } = require("node:module");
 const { pathToFileURL } = require("node:url");
 
-register("./hooks_.js", pathToFileURL(__filename));
-
-// import { register } from "node:module";
-// import { pathToFileURL } from "node:url";
-
-// register("./compile/hooks.js", pathToFileURL("./"));
+register("./hooks.js", pathToFileURL(__filename));
