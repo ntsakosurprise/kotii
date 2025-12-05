6ee496a6c04ead60c223896713a224276e28d4df
ntsakosurprise <mashelesepru@gmail.com>
2025-12-05 06:49:55 +0200

    feat(kotii-runtime): add re-exports

--- packages/kotii/app.js
+++ packages/kotii/app.js
@@ -1,3 +1,5 @@
-import { app } from "kotii-runtime";
-console.log("THE APP KOTII-RUNTIME", app);
-export default app;
+// import { app } from "kotii-runtime";
+// console.log("THE APP KOTII-RUNTIME", app);
+// export default app;
+export * from "kotii-runtime/kotii-land/app.js";
+export { default } from "kotii-runtime/kotii-land/app.js";
