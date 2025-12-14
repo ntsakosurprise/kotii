efacc145b9d8bbba0a138cdf4264f3dff307ad31
ntsakosurprise <mashelesepru@gmail.com>
2025-12-14 04:14:47 +0200

    feat(kotii-creation-time):  add kotii-internal package

--- packages/kotii-creation-time/compile/hooks.js
+++ packages/kotii-creation-time/compile/hooks.js
@@ -58,7 +58,7 @@ const KOTII_INTERNAL_ALIASES = {
   //   alias: "/kotii-user-land-aliase/src/store/index",
   //   value: "/src/store/index",
   // },
-  "@kotii/_internal/land": `${workdir}/node_modules/kotii-creation-time/kotii-land/dev/index`,
+  "@kotii/_internal/land": `${workdir}/node_modules/kotii-internal/dist/index`,
   "@kotii/_internal/plugins": `${workdir}/node_modules/kotii-creation-time/plugins/index`,
   "@kotii/_internal/root": `${workdir}/node_modules/kotii-creation-time/kotii_paths`,
 };
