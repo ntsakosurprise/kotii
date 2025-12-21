df6d564e7106912144444003f8d77b2f7b4c6e69
ntsakosurprise <mashelesepru@gmail.com>
2025-12-21 05:08:12 +0200

    refactor(kotii-internal):  create subpath exports
    Exposing directory as a way to access files in this package was not safe and clean.
    
    We have refactored the exportation to ensure clean and safe exports.

--- packages/kotii/app.js
+++ packages/kotii/app.js
@@ -1,5 +1,5 @@
 // import { app } from "kotii-runtime";
 // console.log("THE APP KOTII-RUNTIME", app);
 // export default app;
-export * from "kotii-runtime/kotii-land/app.js";
-export { default } from "kotii-runtime/kotii-land/app.js";
+export * from "kotii-internal/app";
+export { default } from "kotii-internal/app";
