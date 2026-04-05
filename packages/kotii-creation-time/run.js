443a88c91ea61243993262a946f158d02384216a
ntsakosurprise <mashelesepru@gmail.com>
2026-04-05 06:45:00 +0200

    chore(kotii-creation-time): set production environment for non-dev modes

--- packages/kotii-creation-time/run.js
+++ packages/kotii-creation-time/run.js
@@ -6,14 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
 const parentURL = pathToFileURL(__filename);
-export default () => {
-  register("./compile/hooks.js", parentURL);
-  getAndSetEnvironmentVariables(
-    process.env.NODE_ENV
-      ? process.env.NODE_ENV != "development"
-        ? "development"
-        : process.env.NODE_ENV
-      : "development"
+export default (mode) => {
+  process.env.KOTII_MODE = mode;
+  let environment = "";
+  // if(!environment){
+  //   console.log("RUN ENV SET", environment)
+  //   environment = "development"
+  // }else{
+  //   mode !== "dev" ? environment = "production" : environment = "development"
+  // }
+  mode !== "dev" ? (environment = "production") : (environment = "development");
+  process.env.NODE_ENV = environment;
+  console.log(
+    "RUN ENVIRONMENT",
+    environment,
+    process.env.NODE_ENV,
+    "THE MODE",
+    process.env.KOTII_MODE
   );
+  register("./compile/hooks.js", parentURL);
+
+  getAndSetEnvironmentVariables(environment);
   import("./kotii-land/app.js");
 };
