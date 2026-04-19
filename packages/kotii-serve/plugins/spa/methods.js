05e6469ac439d78def495a6f0aadc68eaea27c89
ntsakosurprise <mashelesepru@gmail.com>
2026-04-19 05:13:50 +0200

    chore(kotii-serve): add catchall handler

--- packages/kotii-serve/plugins/spa/methods.js
+++ packages/kotii-serve/plugins/spa/methods.js
@@ -5,180 +5,55 @@ import path from "path";
 methods.init = function () {
   this.listens({
     "run-spa": this.handleSpaCommand.bind(this),
+    "catch-all": this.handleCatchAll.bind(this),
   });
 };
 methods.handleSpaCommand = function (data) {
   const self = this;
-  const setCall = data.callback;
+  self["callback"] = data.callback;
   const { folder = "build" } = data;
+  self.folderName = folder;
 
   self.emit({
     type: "config-manual",
     data: {
       payload: {
-        configs: {
-          router: [{}],
+        customKickOff: true,
+        config: {
+          router: [
+            {
+              catchAll: true,
+            },
+          ],
           domain: [{ name: "static", set: folder }],
           cluster: { workers: 1, spawn: false },
-          // server: serverConfig,
+          server: "server",
         },
       },
-      callback: (data) => {
-        // self.debug("SENDING A RESTART SIGNAL",process.env?.CUSTOM_RESTART)
-        //     if(process.env?.CUSTOM_RESTART){
-        //       self.debug("SENDING A RESTART SIGNAL")
-        //       process.env.CUSTOM_RESTART = "false"
-        //       self.notifyClient({
-        //         name: "kotii-client-reload",
-        //         vendor: "kotii",
-        //       });
-        //     }
-        // callback(data.message);
-      },
+      callback: (data) => {},
     },
   });
 };
-
-methods.doStartUp = function (data) {
+methods.handleCatchAll = function (data) {
   const self = this;
-  const pao = self.pao;
-  const getWorkingDir = pao.pa_getWorkingFolder;
-  const loadFile = pao.pa_loadFile;
-  const readFileSync = pao.pa_readFileSync;
-  return new Promise((resolve, reject) => {
-    let nodeModulesOrUserLand = path.resolve(getWorkingDir(), "..");
-    let configFolder = getWorkingDir();
-    self.debug("CONFIG FOLDER", configFolder);
 
-    // if (nodeModulesOrUserLand.indexOf("/packages") >= 0) {
-    //   configFolder = `${nodeModulesOrUserLand}/kotii-templates/javascript/ssr`;
-    // }
-    let rootFiles = [];
-    const files = fs.readdirSync(configFolder, { recursive: true });
-
-    for (let fofi = 0; fofi <= files.length; fofi++) {
-      self.debug("FILES");
-      let filePath = `${configFolder}${path.sep}${files[fofi]}`;
-      self.debug("THE FILE PATH", filePath);
-      if (
-        fs.statSync(filePath).isDirectory() &&
-        filePath.indexOf(".kotii-land") >= 0
-      ) {
-        rootFiles.push({
-          folder: files[fofi],
-          fullPath: filePath,
-          buildRoot: path.resolve(filePath, ".."),
-        });
-        break;
-      }
-    }
-
-    let rootStats = rootFiles[0];
-    self.debug("GETTING THE WORKING DIR", configFolder);
-    self.debug("THE FILES", files);
-    self.debug("THE ROOT FILES", rootFiles);
-
-    self.getEnvVariables().then((envs) => {
-      self.debug("THE ENVS", envs);
-
-      self
-        .doProdRoutes({
-          path: {
-            appSrc: `${rootFiles[0].buildRoot}/src`,
-            isProductionRequest: true,
-          },
-        })
-        .then((routes) => {
-          self.debug("The Retrieved Routes", routes);
-
-          if (rootFiles.length > 0) {
-            loadFile(`${rootStats.buildRoot}${path.sep}.config.js`).then(
-              (config) => {
-                let domain = config.domain;
-                console.log("FOUND CONFIG SETTINGS", config);
-                console.log("THE ROOT FILES", rootFiles);
-
-                domain.forEach((doma) => {
-                  if (doma.name === "static") {
-                    doma.set = doma?.set
-                      ? `${rootFiles[0].buildRoot}/${doma.set}`
-                      : `${rootFiles[0].buildRoot}/public`;
-                    doma.absolute = true;
-                  }
-                });
-                let pathApiRoot = path.resolve(rootStats.buildRoot, "api");
-                let pathApiConfig = path.resolve(
-                  rootStats.buildRoot,
-                  "api/.config.js"
-                );
-                if (
-                  fs.existsSync(pathApiRoot) &&
-                  fs.existsSync(pathApiConfig)
-                ) {
-                  loadFile(
-                    `${rootStats.buildRoot}${path.sep}api/.config.js`
-                  ).then((apiConfig) => {
-                    self.debug("API CONFIG", apiConfig);
-                    let appConfig = {
-                      ...config,
-                      ...apiConfig,
-                      router: [...apiConfig.router, ...routes],
-                    };
-                    resolve(appConfig);
-                  });
-                } else {
-                  resolve(config);
-                }
-              }
-            );
-          }
-        });
-    });
+  let html = path.resolve(__dirname, self.folderName, "index.html");
+  self.callback({
+    html: html,
   });
-};
 
-methods.createFolder = function (filepath) {
-  const self = this;
-  const pao = self.pao;
-  const makeFolderSync = pao.pa_makeFolderSync;
-  const isExistingDir = pao.pa_isExistingDir;
-  if (isExistingDir(filepath)) fs.rmSync(filepath, { recursive: true });
-  makeFolderSync(filepath);
-  return filepath;
-};
-
-methods.copyFromToFolder = function (from, to, ignores = []) {
-  const self = this;
-
-  self.debug("copying from", from, to);
-  fs.cpSync(from, to, {
-    recursive: true,
-    filter: (fi) => {
-      // self.debug("THE FILE BEING PROCESSED", fi, ignores.includes(fi));
-      // if (ignores.includes(fi)) return true;
-      // let thisToReturn = fi !== ignore;
-      let thisToReturn = !ignores.includes(fi);
-      // self.debug("THIS TO RETURN", thisToReturn);
-      return thisToReturn;
-    },
-  });
-};
-
-methods.doProdRoutes = function (resources) {
-  const self = this;
-  return new Promise((resolve, reject) => {
-    self.emit({
-      type: "create-file-routes",
-      data: {
-        payload: resources,
-        callback: (data) => {
-          self.debug("FILE ROUTES PROCESSED", data);
-          // pResolve({ routes: data.routes, ...resources, ...data });
-          resolve(data.routes);
-        },
-      },
-    });
-  });
+  // self.emit({
+  //   type: "handle-react-spa",
+  //   data: {
+  //     payload: { build: "server-build" },
+  //     callback: (gotValue) => {
+  //       self.debug("SPA GENERATION IS COMPLETED", gotValue, self.callback);
+  //       self.callback({
+  //         html: gotValue,
+  //       });
+  //     },
+  //   },
+  // });
 };
 
 export default methods;
