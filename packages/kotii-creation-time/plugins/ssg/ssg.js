7bbb7143e80586e6698f6c773dfb3745b00ca042
ntsakosurprise <mashelesepru@gmail.com>
2026-04-23 03:16:31 +0200

    chore(kotii-creation-time):  reference static build seo files generators

--- packages/kotii-creation-time/plugins/ssg/ssg.js
+++ packages/kotii-creation-time/plugins/ssg/ssg.js
@@ -29,7 +29,11 @@ class Ssg {
     this.localizeHrefUrl = methods.localizeHrefUrl;
     this.formatAndSaveHtml = methods.formatAndSaveHtml;
     this.localizeAppResources = methods.localizeAppResources;
-    this.this.createOrMergeRobotsTxt = methods.createOrMergeRobotsTxt;
+    this.createSiteMap = methods.createSiteMap;
+    this.sitemapXmlSkeleton = methods.sitemapXmlSkeleton;
+    this.sitemapsXmlSkeleton = methods.sitemapsXmlSkeleton;
+    this.sitemapXmlPageUrlSkeleton = methods.sitemapXmlPageUrlSkeleton;
+    this.createOrMergeRobotsTxt = methods.createOrMergeRobotsTxt;
   }
 }
 export default Ssg;
