0738314d7e9bdc98b095cce5f50aa152e44d3ab2
ntsakosurprise <mashelesepru@gmail.com>
2026-04-23 03:14:08 +0200

    chore(kotii-creation-time): set and pass script modes to runner

--- packages/kotii-creation-time/app-build/index.js
+++ packages/kotii-creation-time/app-build/index.js
@@ -1,4 +1,4 @@
 import run from "../run.js";
 export const build = () => {
-  run();
+  run("build");
 };
--- packages/kotii-creation-time/ssg/index.js
+++ packages/kotii-creation-time/ssg/index.js
@@ -1,4 +1,4 @@
 import run from "../run.js";
 export const ssg = () => {
-  run();
+  run("ssg");
 };
